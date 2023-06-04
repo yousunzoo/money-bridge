5306bb3b156c1b48b686927c1234eb854c581dfc
kjun <kjun_all@naver.com>
2023-06-04 20:32:38 +0900

    conf: reactQueryProvider set

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import ReactQueryProvider from "./ReactQueryProvider";
 import "./globals.css";
 import { Inter } from "next/font/google";
 
@@ -12,7 +13,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={inter.className}>
-        <main>{children}</main>
+        <ReactQueryProvider>{children}</ReactQueryProvider>
       </body>
     </html>
   );

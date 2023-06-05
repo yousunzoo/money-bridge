774958330ef042be61796c98815a76690e1265e2
kjun <kjun_all@naver.com>
2023-06-06 00:02:56 +0900

    conf: 프로젝트 셋업

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-import ReactQueryProvider from "./ReactQueryProvider";
 import "./globals.css";
 import { Inter } from "next/font/google";
 
@@ -12,9 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <ReactQueryProvider>{children}</ReactQueryProvider>
-      </body>
+      <body className={inter.className}></body>
     </html>
   );
 }

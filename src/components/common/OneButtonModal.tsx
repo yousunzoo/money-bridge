2de349036e3dfafaf5480df73925c9afd55ff1d0
yousunzoo <dpfvmffjqm@gmail.com>
2023-06-07 15:43:32 +0900

    Feat: Modal 닫기 기능

--- src/components/common/OneButtonModal.tsx
+++ src/components/common/OneButtonModal.tsx
@@ -9,16 +9,20 @@ interface OneButtonModalProps {
   setIsOpen:Dispatch<SetStateAction<boolean>>
 }
 function OneButtonModal({children, isOpen, setIsOpen}:OneButtonModalProps) {
-  const portalDiv = document.getElementById('#modal-root')
 
   useEffect(()=>{
     document.body.style.overflow = isOpen ? 'hidden' : 'initial'
   },[isOpen])
+
+  const handleClose = () => {
+    setIsOpen(false)
+  }
   if(!isOpen) return
   return (
     <div className='fixed top-0 left-0 w-full h-full'>
       <ModalOverlay />
-      <div className='absolute w-80 h-60 p-4 bg-white rounded-xl top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
+      <div className='fixed w-80 h-60 p-4 bg-white rounded-xl top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
+        <button onClick={handleClose} className='absolute right-4'>닫기</button>
         <p>{children}</p>
       </div>
     </div>

ebeda8dc6dc0f255cca588567e0483a875f920b8
LeeByoungWook <lbw3973@naver.com>
2023-06-07 21:44:52 +0900

    Feat: closeButton의 유무를 props로 전달

--- src/components/common/LocationCard.tsx
+++ src/components/common/LocationCard.tsx
@@ -6,7 +6,7 @@ import { Map, MapMarker } from "react-kakao-maps-sdk";
 
 const KAKAO_SDK_URL = "//dapi.kakao.com/v2/maps/sdk.js?appkey=56706da76ae397a2378c593f91e2bafb&autoload=false";
 
-function LocationCard({ lat, lng }: { lat: number; lng: number }) {
+function LocationCard({ lat, lng, closeButton }: { lat: number; lng: number; closeButton: boolean }) {
   const [display, setDisplay] = useState(true);
   const mapRef = useRef<HTMLDivElement>(null);
   mapRef.current?.classList.add(display ? "block" : "hidden");
@@ -18,9 +18,11 @@ function LocationCard({ lat, lng }: { lat: number; lng: number }) {
         <Map center={{ lat, lng }} style={{ width: "100%", height: "100%" }}>
           <MapMarker position={{ lat, lng }}></MapMarker>
         </Map>
-        <button className="absolute right-[20px] top-[10px] z-10" onClick={() => setDisplay(!display)}>
-          X
-        </button>
+        {closeButton && (
+          <button className="absolute right-[20px] top-[10px] z-10" onClick={() => setDisplay(!display)}>
+            X
+          </button>
+        )}
       </div>
       {/* <input className="formInput" placeholder="입력" /> */}
     </>

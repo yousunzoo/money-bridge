1299e15f27984a3296455d3a49dc771843493ede
yousunzoo <dpfvmffjqm@gmail.com>
2023-06-07 16:21:15 +0900

    Feat: ButtonModal 테스트용 컴포넌트 작성

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,17 +1,23 @@
 'use client'
 
 import React, { useState } from 'react'
-import OneButtonModal from './common/OneButtonModal'
+import ButtonModal from './common/ButtonModal'
 
 function Test() {
   const [isOpen, setIsOpen] = useState(false)
-  console.log(isOpen)
+
+  // 취소 버튼이 있는 경우에만 cancelText, cancelFn 작성
+  // confirmFn, cancelText, cancelFn은 선택값!
+  const modalContents = {
+    content: '인증코드 시간이 만료되었습니다. 인증코드를 재발송 해주세요.',
+    confirmText: '인증 코드 재발송',
+    cancelText:'취소',
+    confirmFn: () => console.log('send'),
+  }
   return (
     <>
     <button className='px-2 bg-yellow-400 cursor-pointer' onClick={()=>setIsOpen(true)}>test</button>
-  <OneButtonModal isOpen={isOpen} setIsOpen={setIsOpen}>
-    확인
-  </OneButtonModal>
+    <ButtonModal modalContents={modalContents} isOpen={isOpen} setIsOpen={setIsOpen} />
     </>
   )
 }

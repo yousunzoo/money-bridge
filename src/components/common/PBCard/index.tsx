d3202d57d943a8844a764ab26e19e3a321543975
hyeon17 <janghyeon8725@naver.com>
2023-06-07 20:08:46 +0900

    feat: props, 구조 작업 추가

--- src/components/common/PBCard/index.tsx
+++ src/components/common/PBCard/index.tsx
@@ -1,32 +1,36 @@
+import Image from "next/image";
 import React from "react";
 
 function PBCard({ props }: any) {
-  const { id, profile, name, companyName, intro, career, speciality1, speciality2, reservCount, reviewCount } = props;
   return (
-    <div id={id}>
-      <div>
-        <div>{profile}프로필</div>
-        <div>
-          <div>{name}이름</div>
-          <div>{companyName}소속</div>
+    <>
+      {props.map((item: any) => (
+        <div id={item.id} key={item.id}>
           <div>
-            {career}
-            {speciality1}
-            {speciality2}
-            전문분야/경력
+            <div><Image src={item.profile} alt="프로필"/>프로필</div>
+            <div>
+              <div>{item.name}이름</div>
+              <div>{item.companyName}소속</div>
+              <div>
+                {item.career}
+                {item.speciality1}
+                {item.speciality2}
+                전문분야/경력
+              </div>
+            </div>
+            <button>북마크</button>
+          </div>
+          <div>{item.intro}한줄 소개</div>
+          <div>
+            <div>
+              <div>{item.reservCount}상담횟수</div>
+              <div>{item.reviewCount}후기</div>
+            </div>
+            <button>정보보기</button>
           </div>
         </div>
-        <button>북마크</button>
-      </div>
-      <div>{intro}한줄 소개</div>
-      <div>
-        <div>
-          <div>{reservCount}상담횟수</div>
-          <div>{reviewCount}후기</div>
-        </div>
-        <button>정보보기</button>
-      </div>
-    </div>
+      ))}
+    </>
   );
 }
 
--- src/components/common/PostCard/index.tsx
+++ src/components/common/PostCard/index.tsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
-function PostCard() {
+function PostCard({ props }: any) {
   return (
-    <div>PostCard</div>
-  )
+    <>
+      {props ? (
+        props.map((item: any) => (
+          <div id={item.id} key={item.id}>
+            <div>
+              <div>
+                <div>
+                  {item.tag1}
+                  {item.tag2}
+                  태그
+                </div>
+                <div>{item.title}제목</div>
+              </div>
+              <button>북마크</button>
+            </div>
+            <div>
+              <div>
+                <div>
+                  {item.pbName}/{item.career}이름/경력
+                </div>
+                <div>{item.msg}한줄 소개</div>
+              </div>
+              <div>
+                <Image src={item.companyLogo} alt="증권사로고" />
+              </div>
+            </div>
+          </div>
+        ))
+      ) : (
+        <div>작성한 콘텐츠가 없습니다</div>
+      )}
+    </>
+  );
 }
 
-export default PostCard
+export default PostCard;

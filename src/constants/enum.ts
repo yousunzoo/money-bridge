84264e6598a7cfc4e96762215a575c94041b2183
hyeon17 <janghyeon8725@naver.com>
2023-06-07 17:11:01 +0900

    conf: enum 작성

---
+++ src/constants/enum.ts
@@ -0,0 +1,105 @@
+export enum CommonROLE {
+  ROLE_USER = "ROLE_USER",
+  ROLE_PB = " ROLE_PB",
+  ROLE_ADMIN = "ROLE_ADMIN",
+}
+
+export enum Propensity {
+  CONSERVATIVE = "CONSERVATIVE",
+  CAUTIOUS = "CAUTIOUS",
+  BALANCED = "BALANCED",
+  AGGRESSIVE = "AGGRESSIVE",
+  SPECULATIVE = "SPECULATIVE",
+}
+
+export enum PBSpecialty {
+  KOREAN_STOCK = "KOREAN_STOCK",
+  US_STOCK = "US_STOCK",
+  DERIVATIVE = "DERIVATIVE",
+  FUND = "FUND",
+  ETF = "ETF",
+  REAL_ESTATE = "REAL_ESTATE",
+  BOND = "BOND",
+  WRAP = "WRAP",
+}
+
+export enum PBStatus {
+  PENDING = "PENDING",
+  ACTIVE = "ACTIVE",
+  INACTIVE = "INACTIVE",
+}
+
+export enum Admin {}
+
+export enum ReservationProcess {
+  APPLY = "APPLY",
+  CONFIRM = "CONFIRM",
+  COMPLETE = "COMPLETE",
+}
+
+export enum ReservationGoal {
+  PROFIT = "PROFIT",
+  RISK = "RISK",
+  TAX = "TAX",
+  PRESERVATION = "PRESERVATION",
+}
+
+export enum ReservationType {
+  VISIT = "VISIT",
+  CALL = "CALL",
+}
+
+export enum ReservationStatus {
+  ACTIVE = "ACTIVE",
+  INACTIVE = "INACTIVE",
+  CANCEL = "CANCEL",
+}
+
+export enum ReservationLocationType {
+  BRANCH = "BRANCH",
+  CALL = "CALL",
+}
+
+export enum ConsultationStyle {
+  METICULOUS = "METICULOUS",
+  KIND = "KIND",
+  PROFESSIONAL = "PROFESSIONAL",
+  FAST = "FAST",
+  HONEST = "HONEST",
+  PRAGMATIC = "PRAGMATIC",
+  DIRECTIONAL = "DIRECTIONAL",
+}
+
+export enum BoardStatus {
+  ACTIVE = "ACTIVE",
+  INACTIVE = "INACTIVE",
+  TEMP = "TEMP",
+}
+
+export enum Adherence {
+  BAD = "BAD",
+  NORMAL = "NORMAL",
+  EXCELLENT = "EXCELLENT",
+}
+
+export enum UserBookmark {}
+export enum PBBookmark {}
+export enum BoardBookmark {}
+export enum Notice {}
+export enum FrequentQuestion {}
+
+export enum QuestionAuthorRole {
+  USER = "USER",
+  PB = "PB",
+}
+
+export enum Answer {}
+export enum AgreementType {
+  REQUIRED = "REQUIRED",
+  OPTIONAL = "OPTIONAL",
+}
+
+export enum Career {}
+export enum Award {}
+export enum Branch {}
+export enum Company {}

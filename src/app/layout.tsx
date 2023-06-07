339471a507a829eddc9f2261dab0b29ca548e730
yousunzoo <dpfvmffjqm@gmail.com>
2023-06-07 14:49:41 +0900

    Fix: layout 에서 children이 두번씩 출력되는 문제 해결 ReactQueryProvider 안에서 main 태그와 children 세팅되도록

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,8 +12,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={noto.className}>
+        <ReactQueryProvider>
         <main>{children}</main>
-        <ReactQueryProvider>{children}</ReactQueryProvider>
+        </ReactQueryProvider>
       </body>
     </html>
   );

ad0fab371a04b7389560fc15368fca806e529b25
yousunzoo <dpfvmffjqm@gmail.com>
2023-06-08 19:50:58 +0900

    Feat: IBubbleSectionProps interface 세팅

--- src/types/common.ts
+++ src/types/common.ts
@@ -11,3 +11,26 @@ export interface ButtonModalProps {
   isOpen: boolean;
   setIsOpen: Dispatch<SetStateAction<boolean>>;
 }
+
+export interface IBubbleSectionProps {
+  step: number;
+  answers: {
+    0: string[] | null;
+    1: string | null;
+    2: string | null;
+    3: string | null;
+    4: string | null;
+    5: string | null;
+  };
+  setAnswers: Dispatch<
+    SetStateAction<{
+      0: string[] | null;
+      1: string | null;
+      2: string | null;
+      3: string | null;
+      4: string | null;
+      5: string | null;
+    }>
+  >;
+  moveToNextStep: () => void;
+}

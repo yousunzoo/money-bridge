e783ade220b6f1eb984f7549c3085a8c5e027294
bwlee <lbw3973@naver.com>
2023-06-08 15:46:33 +0900

    Feat: login페이지 컴포넌트 화

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,13 +1,19 @@
 "use client";
 import TopNav from "@/components/common/TopNav";
-import LoginForm from "@/components/loginPage/LoginForm";
+import ButtonToFind from "@/components/loginPage/ButtonToFind";
+import ButtonToSignUp from "@/components/loginPage/ButtonToSignUp";
+import DoubleInputForm from "@/components/common/DoubleInputForm";
+import KeepLoginButton from "@/components/loginPage/KeepLoginButton";
 import React from "react";
 
 function page() {
   return (
     <>
       <TopNav title="로그인/회원가입" />
-      <LoginForm />
+      <DoubleInputForm type="login" />
+      <KeepLoginButton />
+      <ButtonToSignUp />
+      <ButtonToFind />
     </>
   );
 }

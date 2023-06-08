8df9bb089cfa0e50db829b0cd4635221564545db
yousunzoo <dpfvmffjqm@gmail.com>
2023-06-08 16:45:23 +0900

    Design: SpeechBubble 기본 디자인 세팅

---
+++ src/app/reservation/page.tsx
@@ -0,0 +1,18 @@
+function ReservationPage() {
+  return (
+    <div className="flex w-full flex-col p-6">
+      <div className="chatBubble">
+        <p>
+          PB의 상담예약 페이지입니다!
+          <br />
+          머니브릿지와 함께 정해보아요~
+        </p>
+      </div>
+      <div className="userBubble">
+        <p>네! 좋아요</p>
+      </div>
+    </div>
+  );
+}
+
+export default ReservationPage;

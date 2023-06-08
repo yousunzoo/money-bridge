6625581cc7be681836a4c27510c6827a57894b88
LeeByoungWook <lbw3973@naver.com>
2023-06-09 01:28:32 +0900

    Feat: 이메일 찾기 step2 제작

---
+++ src/components/findEmailPage/InformationCheck.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+function InformationCheck() {
+  return (
+    <div className=" pt-[50px]">
+      <div className="mx-[50px] mb-[25px] mt-[50px] flex flex-col items-center gap-[15px] rounded-[20px] bg-gray-300 p-[20px]">
+        <div>
+          <p>
+            해당 정보가
+            <br />
+            맞으신가요?
+          </p>
+        </div>
+        <div className="flex w-full flex-row justify-between text-xs">
+          <p>이름</p>
+          <p>이병욱</p>
+        </div>
+        <div className="flex w-full flex-row justify-between text-xs">
+          <p>전화번호</p>
+          <p>010-xxxx-xxxx</p>
+        </div>
+        <div className="flex w-full flex-row justify-between text-xs">
+          <p>이메일</p>
+          <p>test@test.com</p>
+        </div>
+      </div>
+      <button className="h-[40px] w-full bg-gray-300">
+        <span className="text-xs font-semibold text-slate-400">로그인 하기</span>
+      </button>
+    </div>
+  );
+}
+
+export default InformationCheck;

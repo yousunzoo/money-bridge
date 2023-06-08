e4ab7e0b4adfe2c2ded5c85751cba985f710bda0
LeeByoungWook <lbw3973@naver.com>
2023-06-08 18:29:57 +0900

    Fix: 에러 수정
    - input에서 checked를 사용할 때, onChange가 필요하다하여 빈 함수 추가

--- src/components/loginPage/KeepLoginButton.tsx
+++ src/components/loginPage/KeepLoginButton.tsx
@@ -4,7 +4,7 @@ function KeepLoginButton() {
   const [autoLogin, setAutoLogin] = useState(false);
   return (
     <div className="flex h-[50px] items-center justify-center gap-1">
-      <input type="radio" checked={autoLogin} onClick={() => setAutoLogin(!autoLogin)} />
+      <input type="radio" checked={autoLogin} onClick={() => setAutoLogin(!autoLogin)} onChange={() => {}} />
       <span className="text-xs font-semibold">로그인 상태 유지하기</span>
     </div>
   );

c6ad58d18be1c86915b0ab51a28de83241738e6a
LeeByoungWook <lbw3973@naver.com>
2023-06-08 12:13:33 +0900

    Feat: 로그인 유효성검사 구현

--- src/components/loginPage/LoginForm.tsx
+++ src/components/loginPage/LoginForm.tsx
@@ -1,32 +1,68 @@
 "use client";
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 
 function LoginForm() {
   const [autoLogin, setAutoLogin] = useState(false);
+  const noticeEmail = useRef<HTMLSpanElement>(null);
+  const noticePassword = useRef<HTMLSpanElement>(null);
+
+  const schema = yup.object().shape({
+    email: yup.string().email().required("email is required"),
+    password: yup.string().min(8).max(15).required("password is required"),
+  });
+
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm({ mode: "onChange" });
+  } = useForm({ mode: "onChange", resolver: yupResolver(schema) });
 
-  const onSubmit = () => {};
+  if (errors.email) {
+    noticeEmail.current?.classList.remove("text-slate-300");
+    noticeEmail.current?.classList.add("text-red-600");
+  } else {
+    noticeEmail.current?.classList.add("text-slate-300");
+    noticeEmail.current?.classList.remove("text-red-600");
+  }
+
+  if (errors.password) {
+    noticePassword.current?.classList.remove("text-slate-300");
+    noticePassword.current?.classList.add("text-red-600");
+  } else {
+    noticePassword.current?.classList.add("text-slate-300");
+    noticePassword.current?.classList.remove("text-red-600");
+  }
+
+  const onSubmit = () => {
+    if (noticeEmail.current?.classList.contains("text-red-600")) {
+      alert("ㅡemailㅡ");
+    } else if (noticePassword.current?.classList.contains("text-red-600")) {
+      alert("ㅡpasswordㅡ");
+    }
+  };
 
   return (
     <div className="pt-[100px]">
       <form onSubmit={() => handleSubmit(onSubmit)}>
         <div className="mb-[10px]">
           <h2 className="mb-[20px] font-bold">이메일</h2>
-          <input className="formInput" />
-          <span className="text-xs text-slate-300">* @포함하여 작성해 주세요.</span>
+          <input type="text" className="formInput" {...register("email")} />
+          <span className="text-xs" ref={noticeEmail}>
+            * @포함하여 작성해 주세요.
+          </span>
         </div>
         <div className="mb-[10px]">
           <h2 className="mb-[20px] font-bold">비밀번호</h2>
-          <input className="formInput" />
-          <span className="text-xs text-slate-300">* 8자 이상입니다.</span>
+          <input type="password" className="formInput" {...register("password")} />
+          <span className="text-xs" ref={noticePassword}>
+            * 8자 이상입니다.
+          </span>
         </div>
         {/* <button type="submit" className="h-[40px] w-full bg-gray-300 "> */}
-        <button type="button" className="h-[40px] w-full bg-gray-300 ">
+        <button type="button" className="h-[40px] w-full bg-gray-300" onClick={onSubmit}>
           <span className="text-xs font-semibold text-slate-400">로그인</span>
         </button>
       </form>

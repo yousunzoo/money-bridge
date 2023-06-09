a31fac2db041cc66d64162b342edfb89866396b9
kjun <kjun_all@naver.com>
2023-06-09 22:17:29 +0900

    Feat: 고객관리 페이지 구현

--- src/components/common/CardList/UserReservationList.tsx
+++ src/components/common/CardList/UserReservationList.tsx
@@ -3,15 +3,14 @@ import React from "react";
 import UserReservationItem from "../CardItem/UserReservationItem";
 
 function UserReservationList() {
-  const onClickhandler = () => {
-    alert("zz");
-  };
   return (
     <ul>
-      <UserReservationItem buttonName="확인하기" onClickhandler={onClickhandler}>
-        <div className="font-bold">gg</div>
-        <div>gg</div>
-      </UserReservationItem>
+      {Array.from({ length: 5 }).map((_, index) => (
+        <UserReservationItem buttonName="확인하기" key={index}>
+          <div className="font-bold">gg</div>
+          <div>gg</div>
+        </UserReservationItem>
+      ))}
     </ul>
   );
 }

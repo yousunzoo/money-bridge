7967ae2256bfe11f873adea120cad7408eb1273c
hyeon17 <janghyeon8725@naver.com>
2023-06-09 20:47:37 +0900

    feat: 비로그인 회원 로그인 모달 창 보이게 추가

--- src/components/loungePage/Intro.tsx
+++ src/components/loungePage/Intro.tsx
@@ -1,15 +1,33 @@
 "use client";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { useRoleStore } from "@/store/roleStore";
 import { CommonROLE } from "@/constants/enum";
+import ButtonModal from "@/components/common/ButtonModal";
 
 function Intro() {
   const router = useRouter();
+  const [isOpen, setIsOpen] = useState(false);
+  const [role, setRole] = useState("");
   const { getRole } = useRoleStore();
+  const modalContents = {
+    content: "로그인 후 북마크 저장이 가능합니다",
+    confirmText: "확인",
+    confirmFn: () => {
+      router.push("/login");
+    },
+  };
+  useEffect(() => {
+    const roles = getRole();
+    setRole(roles);
+  }, [role]);
 
   const goToBookMark = () => {
-    router.push("/bookmark");
+    if (getRole() === "") {
+      setIsOpen(true);
+    } else {
+      router.push("/bookmark");
+    }
   };
   const goToPbWrite = () => {
     router.push("/lounge/write");
@@ -20,7 +38,7 @@ function Intro() {
 
   return (
     <div className="flex">
-      <div>
+      <div className="flex">
         <div className="flex flex-1 flex-col">
           <div>라운지</div>
           <div>라운지 소개</div>
@@ -29,7 +47,7 @@ function Intro() {
           아이콘
         </button>
       </div>
-      {getRole() === CommonROLE.PB && (
+      {role === CommonROLE.PB && (
         <div>
           <div>투자자들을 위한 투자 정보 콘텐츠를 작성해보세요</div>
           <div>
@@ -38,6 +56,7 @@ function Intro() {
           </div>
         </div>
       )}
+      {isOpen && <ButtonModal modalContents={modalContents} isOpen={isOpen} setIsOpen={setIsOpen} />}
     </div>
   );
 }

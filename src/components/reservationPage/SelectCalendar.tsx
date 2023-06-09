1d4281e9307386a7d8e48e1e6144e8bf2d7b64e1
yousunzoo <dpfvmffjqm@gmail.com>
2023-06-09 16:54:14 +0900

    Feat: 주말 클릭 불가 처리

--- src/components/reservationPage/SelectCalendar.tsx
+++ src/components/reservationPage/SelectCalendar.tsx
@@ -3,18 +3,41 @@ import "@/styles/selectCalendar.css";
 import { Calendar } from "antd";
 import dayjs from "dayjs";
 import "dayjs/locale/ko";
+import locale from "antd/es/calendar/locale/ko_KR";
+
+import { theme } from "antd";
+import { SelectCalendarProps } from "@/types/reservation";
+
 dayjs.locale("ko");
 
-function SelectCalendar({ handleSelect }) {
+function SelectCalendar({ handleSelect }: SelectCalendarProps) {
+  const { token } = theme.useToken();
   const disabledDate = (date: dayjs.Dayjs) => {
     const now = dayjs().endOf("d").valueOf();
-    if (date.endOf("d").valueOf() < now) {
+    if (date.endOf("d").valueOf() < now || !isWeekday(date)) {
       return true;
     }
     return false;
   };
 
-  return <Calendar disabledDate={disabledDate} fullscreen={false} onSelect={handleSelect} />;
+  const isWeekday = (date: dayjs.Dayjs) => {
+    return date.day() !== 0 && date.day() !== 6;
+  };
+
+  const dateCellRender = (date: dayjs.Dayjs) => {
+    if (!isWeekday(date)) {
+      return null;
+    }
+  };
+  return (
+    <Calendar
+      locale={locale}
+      cellRender={dateCellRender}
+      disabledDate={disabledDate}
+      fullscreen={false}
+      onSelect={handleSelect}
+    />
+  );
 }
 
 export default SelectCalendar;

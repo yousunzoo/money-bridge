3d620f544d0e7d5ec2e9bafc8b6e7470721c4125
LeeByoungWook <lbw3973@naver.com>
2023-06-10 23:12:55 +0900

    Feat: 로그인 타입 선택페이지 제작

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,25 +1,33 @@
-"use client";
-import TopNav from "@/components/common/TopNav";
-import ButtonToFind from "@/components/loginPage/ButtonToFind";
-import ButtonToSignUp from "@/components/loginPage/ButtonToSignUp";
-import DoubleInputForm from "@/components/common/DoubleInputForm";
-import KeepLoginButton from "@/components/loginPage/KeepLoginButton";
-import React, { useState } from "react";
+import Link from "next/link";
+import React from "react";
 
-function Login() {
-  const [inputs, setInputs] = useState({
-    first: "",
-    second: "",
-  });
+function SelectLoginType() {
   return (
     <>
-      <TopNav title="로그인/회원가입" />
-      <DoubleInputForm type="login" inputs={inputs} setInputs={setInputs} />
-      <KeepLoginButton />
-      <ButtonToSignUp />
-      <ButtonToFind />
+      <div className="ml-[16px] mr-[76px] mt-[86px] text-[#242424]">
+        <p className="font-bold ">처음뵙겠습니다,</p>
+        <p className="font-bold">
+          <span className="text-[32px] text-[#153445]">MONEY BRIDGE </span>입니다.
+        </p>
+        <p className="font-normal">내가 찾던 PB와 투자자를 지금 만나보세요 :&#41;</p>
+        <p></p>
+      </div>
+      <div className="mx-[16px] mt-[200px] flex flex-col gap-[16px]">
+        <Link
+          href={"/login/user"}
+          className="flex h-[56px] w-full items-center  justify-center rounded-[8px] bg-[#453015] text-[20px] font-bold leading-[28px] text-white"
+        >
+          일반회원
+        </Link>
+        <Link
+          href={"/login/pb"}
+          className="flex h-[56px] w-full items-center  justify-center rounded-[8px] bg-[#153445] text-[20px] font-bold leading-[28px] text-white"
+        >
+          PB
+        </Link>
+      </div>
     </>
   );
 }
 
-export default Login;
+export default SelectLoginType;

26dd4d429cdee5cc1e333de57fada6bbcb889714
hyeon17 <janghyeon8725@naver.com>
2023-06-10 17:56:42 +0900

    Fix: 경로 수정, props 네이밍 수정

--- src/app/lounge/page.tsx
+++ src/app/lounge/page.tsx
@@ -1,24 +1,20 @@
-
 import Header from "@/components/common/Header/Header";
 import Search from "@/components/loungePage/Search";
 import Intro from "@/components/loungePage/Intro";
 import PbRecommend from "@/components/loungePage/PbRecommend";
-import NewHot from '@/mocks/hyeon17/Lounge/newandhot.json'
-import All from '@/mocks/hyeon17/Lounge/all.json'
+import NewAndHot from "@/mocks/hyeon17/Lounge/newandhot.json";
+import All from "@/mocks/hyeon17/Lounge/all.json";
 import React from "react";
 import Content from "@/components/loungePage/Content";
 
 function Lounge() {
-  const newHot = NewHot.data;
-  const all = All.data.list;
-
   return (
-    <div className="flex w-full flex-col mb-5">
+    <div className="mb-5 flex w-full flex-col">
       <Header />
       <Intro />
       <Search />
       <PbRecommend />
-      <Content newHot={newHot} all={all} />
+      <Content NewAndHot={NewAndHot} All={All} />
     </div>
   );
 }

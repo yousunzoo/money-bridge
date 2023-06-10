c1f5b3a8c76d15705fa752fc36140e68aeb7c03c
LeeByoungWook <lbw3973@naver.com>
2023-06-11 02:38:17 +0900

    Feat: 안내 문구 추가

--- src/app/findEmail/page.tsx
+++ src/app/findEmail/page.tsx
@@ -12,8 +12,11 @@ function FindEmail() {
   });
   return (
     <>
-      <TopNav title="이메일 찾기" path="/login" />
-      {nextStep ? (
+      <TopNav title="이메일 찾기" hasBack />
+      <p className="ml-[16px] mt-[56px] pb-[16px] text-[20px] font-bold leading-[28px]">
+        {nextStep ? "해당하는 정보를 선택해 주세요." : "가입할 때 등록한 정보를 입력해 주세요."}
+      </p>
+      {!nextStep ? (
         <InformationCheck inputs={inputs} />
       ) : (
         <DoubleInputForm type="findEmail" inputs={inputs} setInputs={setInputs} setNextStep={setNextStep} />

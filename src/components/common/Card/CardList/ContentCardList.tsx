d5d338d8448b929da2b662a22620034d55b4b6e0
hyeon17 <janghyeon8725@naver.com>
2023-06-10 17:56:04 +0900

    Fix: props데이터 형식에 따라 동작하세 수정

--- src/components/common/Card/CardList/ContentCardList.tsx
+++ src/components/common/Card/CardList/ContentCardList.tsx
@@ -2,8 +2,10 @@
 import React, { useState, useEffect, useRef } from "react";
 import PostCardItem from "@/components/common/Card/CardItem/ContentCardItem";
 
-function ContentCardList({props}: any) {
-  const [items, setItems] = useState(props.slice(0, 10));
+function ContentCardList({ props }: any) {
+  const dataList = props.data ? props.data.list : props;
+  const data = props.data ? props.data : props;
+  const [items, setItems] = useState(dataList.slice(0, 10));
   const [isLastPage, setIsLastPage] = useState(false);
   const observerRef = useRef<HTMLDivElement | null>(null);
 
@@ -36,11 +38,11 @@ function ContentCardList({props}: any) {
   const loadMoreItems = () => {
     const startIndex = items.length;
     const endIndex = startIndex + 10;
-    const newItems = props.slice(startIndex, endIndex);
+    const newItems = dataList.slice(startIndex, endIndex);
     setItems((prevItems: any) => [...prevItems, ...newItems]);
 
     // 현재 페이지가 마지막 페이지인지 확인
-    if (props.data.curPage === props.data.totalPages - 1) {
+    if (data.curPage === data.totalPages - 1) {
       setIsLastPage(true);
     }
   };

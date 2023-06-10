d908caf68f612dd80176bdc385fcccff840a5414
hyeon17 <janghyeon8725@naver.com>
2023-06-10 19:42:21 +0900

    Fix: div 삭제

--- src/app/bookmark/page.tsx
+++ src/app/bookmark/page.tsx
@@ -4,13 +4,13 @@ import TopNav from "@/components/common/TopNav";
 
 function BookMark() {
   return (
-    <div>
+    <>
       <TopNav title="북마크"/>
       <div>
         <Link href="/bookmark/content">콘텐츠</Link>
         <Link href="/bookmark/pb">PB</Link>
       </div>
-    </div>
+    </>
   );
 }
 

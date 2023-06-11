427110dcf0088694e2eab47b205dfc45324f3f45
LeeByoungWook <lbw3973@naver.com>
2023-06-11 21:12:18 +0900

    Feat: 변수명 풀네임으로 변경 및 맵 사이즈 변경

--- src/components/common/LocationCard.tsx
+++ src/components/common/LocationCard.tsx
@@ -1,12 +1,12 @@
 "use client";
 import { Map, MapMarker } from "react-kakao-maps-sdk";
 
-function LocationCard({ lat, lng }: { lat: number; lng: number }) {
+function LocationCard({ latitude, longitude }: { latitude: number; longitude: number }) {
   return (
     <>
-      <div className="relative !z-0 h-[300px] w-full">
-        <Map center={{ lat, lng }} style={{ width: "100%", height: "100%" }}>
-          <MapMarker position={{ lat, lng }}></MapMarker>
+      <div className="relative !z-0 h-[140px] w-[320px]">
+        <Map center={{ lat: latitude, lng: longitude }} style={{ width: "100%", height: "100%" }}>
+          <MapMarker position={{ lat: latitude, lng: longitude }}></MapMarker>
         </Map>
       </div>
     </>

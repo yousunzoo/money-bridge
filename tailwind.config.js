3ec84668ef7a431a73632a7e30a1df2235c9d639
yousunzoo <dpfvmffjqm@gmail.com>
2023-06-11 23:08:54 +0900

    Design: tailwind.config 폰트 사이즈 세팅

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,23 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      "primary-normal": "#153445",
+      "primary-light": "#3A7391",
+      "secondary-normal": "#91580F",
+      "secondary-light": "#CC9454",
+      "secondary-heavy": "#453015",
+      "status-alert": "#EB5147",
+      "status-caution": "#FA9538",
+      "status-positive": "#0090FF",
+      "background-primary": "#F3F3F3",
+      "background-secondary": "#EBEBEB",
+      "background-normal": "#EEEEEE",
+    },
+    fontSize:{
+      '3xl': '2rem',
+      '5xl':'2.5rem'
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",

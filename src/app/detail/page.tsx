9b0a8c807ccb565d3de4ccf277b74a57b6d85daa
hyeon17 <janghyeon8725@naver.com>
2023-06-12 06:46:13 +0900

    Feat: about 컴포넌트 추가

--- src/app/detail/page.tsx
+++ src/app/detail/page.tsx
@@ -5,6 +5,7 @@ import Content from "@/components/pbdetailPage/Content";
 import Intro from "@/components/pbdetailPage/Intro";
 import profile from "@/mocks/hyeon17/PbDetail/profile.json";
 import { useRoleStore } from "@/store/roleStore";
+import About from "@/components/pbdetailPage/About";
 
 function PbDetail() {
   const data = profile.data;
@@ -18,25 +19,35 @@ function PbDetail() {
     reserveCount: data.reserveCount,
     reviewCount: data.reviewCount,
   };
+
   const contentData = {
-    name: data.name,
-    id: data.id,
     intro: data.intro,
-    address: data.address,
-    branchName: data.branchName,
-    companyName: data.companyName,
     speciality1: data.speciality1,
     speciality2: data.speciality2,
     career: data.career,
     award: data.award,
   };
+
+  const aboutData = {
+    name: data.name,
+    id: data.id,
+    address: data.address,
+    branchName: data.branchName,
+    companyName: data.companyName,
+  };
+
   const { getRole } = useRoleStore();
 
   return (
     <>
       <TopNav title="PB 상세프로필" hasBack={true} />
       <Intro introData={introData} />
-      {getRole() === "" ? null : <Content contentData={contentData} />}
+      {getRole() === "" ? null : (
+        <>
+          <Content contentData={contentData} edit={false} />
+          <About aboutData={aboutData} />
+        </>
+      )}
     </>
   );
 }

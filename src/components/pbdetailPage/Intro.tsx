1d213edea148b21ca82010a5fd884802975265e2
hyeon17 <janghyeon8725@naver.com>
2023-06-11 23:53:42 +0900

    Feat: 로고 클릭시 증권사로 이동

--- src/components/pbdetailPage/Intro.tsx
+++ src/components/pbdetailPage/Intro.tsx
@@ -2,12 +2,16 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 
-function Intro({introData}: any) {
+function Intro({ introData }: any) {
   const { id, profile, name, branchName, msg, companyName, reserveCount, reviewCount } = introData;
+  const goToCompany = () => {
+    console.log("goToCompany");
+  };
+
   return (
     <div id={id}>
       <div>
-        <div>로고</div>
+        <div onClick={goToCompany}>로고</div>
         <Image src={profile} alt="프로필 이미지" width={30} height={30}></Image>
         <div>{msg}</div>
       </div>
@@ -18,8 +22,8 @@ function Intro({introData}: any) {
           {branchName}점
         </div>
         <div>
-          <div>총 상담횟수{reserveCount}회</div>
-          <div>상담후기 {reviewCount}건</div>
+          <div>총 상담횟수{reserveCount ? reserveCount : 0}회</div>
+          <div>상담후기 {reviewCount ? reviewCount : 0}건</div>
         </div>
       </div>
       <div>

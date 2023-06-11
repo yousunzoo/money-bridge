d46014c8ce0241aa3aa1879656290cdccc58ee7d
LeeByoungWook <lbw3973@naver.com>
2023-06-11 17:48:56 +0900

    Feat: 타이틀 문구 조건부 렌더링 처리

--- src/app/findEmail/page.tsx
+++ src/app/findEmail/page.tsx
@@ -21,28 +21,35 @@ function FindEmail() {
   return (
     <>
       <TopNav title="이메일 찾기" hasBack />
-      <p className="mb-[40px] mt-[56px] px-[16px] text-[20px] font-bold leading-[28px]">
-        {nextStep ? "해당하는 정보를 선택해 주세요." : "가입할 때 등록한 정보를 입력해 주세요."}
-      </p>
       {nextStep ? (
-        <div className="px-[16px]">
-          {[1, 2].map(item => (
-            <div key={item} className="mb-[24px] flex items-center gap-[12px]">
-              <button
-                className={`h-[24px] w-[24px] cursor-default rounded-full border-[2px]  ${
-                  isChecked ? "border-[#153455] bg-[#153455]" : "border-[#dfdfdf] bg-transparent"
-                }`}
-                onClick={clickInform}
-              ></button>
-              <InformationCheck inputs={inputs} />
-            </div>
-          ))}
-          <button type="button" className="mt-[54px] h-[56px] w-full rounded-[8px] bg-[#153445]" onClick={clickLogin}>
-            <span className="text-[20px] font-bold leading-[28px] text-white">로그인</span>
-          </button>
-        </div>
+        <>
+          <p className="mb-[40px] mt-[56px] px-[16px] text-[20px] font-bold leading-[28px]">
+            해당하는 정보를 선택해 주세요.
+          </p>
+          <div className="px-[16px]">
+            {[1, 2].map(item => (
+              <div key={item} className="mb-[24px] flex items-center gap-[12px]">
+                <button
+                  className={`h-[24px] w-[24px] cursor-default rounded-full border-[2px]  ${
+                    isChecked ? "border-[#153455] bg-[#153455]" : "border-[#dfdfdf] bg-transparent"
+                  }`}
+                  onClick={clickInform}
+                ></button>
+                <InformationCheck inputs={inputs} />
+              </div>
+            ))}
+            <button type="button" className="mt-[54px] h-[56px] w-full rounded-[8px] bg-[#153445]" onClick={clickLogin}>
+              <span className="text-[20px] font-bold leading-[28px] text-white">로그인</span>
+            </button>
+          </div>
+        </>
       ) : (
-        <DoubleInputForm type="findEmail" inputs={inputs} setInputs={setInputs} setNextStep={setNextStep} />
+        <>
+          <p className="mb-[40px] mt-[56px] px-[16px] text-[20px] font-bold leading-[28px]">
+            가입할 때 등록한 정보를 입력해 주세요.
+          </p>
+          <DoubleInputForm type="findEmail" inputs={inputs} setInputs={setInputs} setNextStep={setNextStep} />
+        </>
       )}
     </>
   );

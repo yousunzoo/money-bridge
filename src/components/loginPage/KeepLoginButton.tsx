091550c42191137c836167e482d56eb6064950de
LeeByoungWook <lbw3973@naver.com>
2023-06-11 18:30:19 +0900

    Feat: 로그인 유지하기 버튼 체크박스 이미지 추가

--- src/components/loginPage/KeepLoginButton.tsx
+++ src/components/loginPage/KeepLoginButton.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import Image from "next/image";
+import check from "/public/assets/images/check.svg";
 
 function KeepLoginButton() {
   const [autoLogin, setAutoLogin] = useState(false);
@@ -9,11 +11,13 @@ function KeepLoginButton() {
   return (
     <div className="flex h-[50px] items-center justify-center gap-1">
       <button
-        className={`h-[24px] w-[24px] cursor-default rounded-full border-[2px] ${
+        className={`flex h-[24px] w-[24px] cursor-default items-center justify-center rounded-full border-[2px] ${
           autoLogin ? "border-[#153455] bg-[#153455]" : "border-[#dfdfdf] bg-transparent"
         }`}
         onClick={clickRememberLoginStatus}
-      ></button>
+      >
+        <Image src={check} alt="check" />
+      </button>
       <span className="text-xs font-semibold">로그인 상태 유지하기</span>
     </div>
   );

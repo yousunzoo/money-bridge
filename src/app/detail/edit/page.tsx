cb6955aaa3cbeae11708e0e7ecbd31ccc83abb1b
hyeon17 <janghyeon8725@naver.com>
2023-06-12 06:46:56 +0900

    Feat: pb상세 수정하기 페이지

--- src/app/detail/edit/page.tsx
+++ src/app/detail/edit/page.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+"use client";
+import React from "react";
+import TopNav from "@/components/common/TopNav";
+import Content from "@/components/pbdetailPage/Content";
+import Intro from "@/components/pbdetailPage/Intro";
+import profile from "@/mocks/hyeon17/PbDetail/profile.json";
 
 function PbDetailEdit() {
+  const data = profile.data;
+  const introData = {
+    id: data.id,
+    profile: data.profile,
+    name: data.name,
+    branchName: data.branchName,
+    msg: data.msg,
+    companyName: data.companyName,
+    reserveCount: data.reserveCount,
+    reviewCount: data.reviewCount,
+  };
+
+  const contentData = {
+    intro: data.intro,
+    speciality1: data.speciality1,
+    speciality2: data.speciality2,
+    career: data.career,
+    award: data.award,
+  };
+
   return (
-    <div>PbDetailEdit</div>
-  )
+    <>
+      <TopNav title="PB 상세프로필" hasBack={true} />
+      <Intro introData={introData} />
+      <Content contentData={contentData} edit={true} />
+    </>
+  );
 }
 
-export default PbDetailEdit
+export default PbDetailEdit;

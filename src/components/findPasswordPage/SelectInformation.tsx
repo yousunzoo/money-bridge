94dafa11d9cd60306445cd3b527d8bc6d6f8d809
LeeByoungWook <lbw3973@naver.com>
2023-06-13 04:38:31 +0900

    Feat: 비밀번호 찾기 정보확인 컴포넌트 구현

---
+++ src/components/findPasswordPage/SelectInformation.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import InformationCheck from "../findEmailPage/InformationCheck";
+import TopNav from "../common/TopNav";
+import { useRouter } from "next/navigation";
+
+function SelectInformation() {
+  const router = useRouter();
+  return (
+    <>
+      <TopNav title="비밀번호 찾기" hasBack backGroundWhite />
+      <div className="px-[16px]">
+        <p className="mb-[40px] mt-[56px] text-[20px] font-bold leading-[28px]">해당하는 정보를 선택해주세요.</p>
+        <InformationCheck />
+        <button
+          className="mb-[96px] mt-[266px] h-[56px] w-full rounded-[8px] bg-[#153445] text-[20px] font-bold leading-[28px] text-white"
+          onClick={() => router.push("/findPassword/4")}
+        >
+          비밀번호 재설정
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default SelectInformation;

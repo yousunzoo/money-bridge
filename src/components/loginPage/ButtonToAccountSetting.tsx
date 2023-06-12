2903d030fa4ea2020ca6a56b72be640d63877699
LeeByoungWook <lbw3973@naver.com>
2023-06-13 04:39:54 +0900

    Feat: 비밀번호 찾기 Link href 수정

--- src/components/loginPage/ButtonToAccountSetting.tsx
+++ src/components/loginPage/ButtonToAccountSetting.tsx
@@ -6,7 +6,7 @@ function ButtonToFind() {
     <div className="mt-[140px] flex h-[20px] items-center justify-center gap-[16px] text-[14px] leading-[20px] text-[#565656] underline">
       <Link href="/join">회원가입</Link>
       <Link href="/findEmail">이메일 찾기</Link>
-      <Link href="/findPassword">비밀번호 찾기</Link>
+      <Link href="/findPassword/1">비밀번호 찾기</Link>
     </div>
   );
 }

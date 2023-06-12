d6c8794c46fbe2c1807df12830a8dc4b7bac26fc
LeeByoungWook <lbw3973@naver.com>
2023-06-13 04:34:38 +0900

    Remove: 파일 삭제

--- src/app/findPassword/[slug]/authentication.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Authentication() {
-  return <div>authentication</div>;
-}
-
-export default Authentication;
--- src/app/findPassword/[slug]/inputInformation.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function InputInformation() {
-  return <div>inputInformation</div>;
-}
-
-export default InputInformation;

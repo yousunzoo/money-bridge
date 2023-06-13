598e636d1c55f13de0254a63473f54193d3bcd91
hyeon17 <janghyeon8725@naver.com>
2023-06-13 21:19:56 +0900

    Fix: 공유기능 주석처리

--- src/components/pbdetailPage/Intro.tsx
+++ src/components/pbdetailPage/Intro.tsx
@@ -6,7 +6,7 @@ import { useRoleStore } from "@/store/roleStore";
 import BlurModal from "@/components/common/Modal/BlurModal";
 import ButtonModal from "@/components/common/ButtonModal";
 import { usePathname, useRouter } from "next/navigation";
-import shareKakao from "@/utils/shareKakao";
+// import shareKakao from "@/utils/shareKakao";
 
 function Intro({ introData, edit }: { introData: any; edit: boolean }) {
   const { profile, name, isBookmarked, branchName, msg, companyId,companyName, companyLogo, reserveCount, reviewCount } =
@@ -43,7 +43,7 @@ function Intro({ introData, edit }: { introData: any; edit: boolean }) {
     confirmText: "카카오톡으로 공유",
     cancelText: "링크 복사",
     confirmFn: () => {
-      shareKakao(base, "테스트", "https://www.google.com", "/public/assets/images/default_profile.png");
+      // shareKakao(base, "테스트", "https://www.google.com", "/public/assets/images/default_profile.png");
       setIsShareOpen(false);
     },
     cancelFn: () => {
--- src/utils/shareKakao.ts
+++ src/utils/shareKakao.ts
@@ -1,41 +1,40 @@
-"use client";
-import API_KEY from "@/app/layout";
-declare global {
-  interface Window {
-    Kakao: any;
-  }
-}
-const shareKakao = (url: string, title: string, description: string, imageUrl: string) => {
-  if (window.Kakao) {
-    const kakao = window.Kakao;
-    if (!kakao.isInitialized()) {
-      kakao.init(API_KEY);
-    }
+// import API_KEY from "@/app/layout";
 
-    kakao.Share.sendDefault({
-      objectType: "feed",
-      content: {
-        title: title,
-        description: description,
-        imageUrl: imageUrl,
-        imageWidth: 200,
-        imageHeight: 100,
-        link: {
-          mobileWebUrl: url,
-          webUrl: url,
-        },
-      },
-      buttons: [
-        {
-          title: "보러 가기",
-          link: {
-            mobileWebUrl: url,
-            webUrl: url,
-          },
-        },
-      ],
-    });
-  }
-};
+//   interface Window {
+//     Kakao: any;
+//   }
 
-export default shareKakao;
+// const shareKakao = (url: string, title: string, description: string, imageUrl: string) => {
+//   if (Window.Kakao) {
+//     const kakao = Window.Kakao;
+//     if (!kakao.isInitialized()) {
+//       kakao.init(API_KEY);
+//     }
+
+//     kakao.Share.sendDefault({
+//       objectType: "feed",
+//       content: {
+//         title: title,
+//         description: description,
+//         imageUrl: imageUrl,
+//         imageWidth: 200,
+//         imageHeight: 100,
+//         link: {
+//           mobileWebUrl: url,
+//           webUrl: url,
+//         },
+//       },
+//       buttons: [
+//         {
+//           title: "보러 가기",
+//           link: {
+//             mobileWebUrl: url,
+//             webUrl: url,
+//           },
+//         },
+//       ],
+//     });
+//   }
+// };
+
+// export default shareKakao;

00511a83862d0b41d4a5fcfb1c955605e419be19
kjun <kjun_all@naver.com>
2023-06-15 14:11:38 +0900

    Feat: 스타일링 코드 상수 설정

--- src/app/myCounseling/UserConultationStatus.tsx
+++ src/app/myCounseling/UserConultationStatus.tsx
@@ -7,6 +7,10 @@ interface ConsultationStatue {
   completeCount: number;
   isNewComplete: boolean;
 }
+
+const boxStyle =
+  "flex h-[68] w-[104px] flex-col items-center rounded-md border-1 border-b-secondary-heavy bg-white py-[14px] text-center  text-sm font-bold text-secondary-heavy";
+
 function UserConsultationStatus({
   applyCount,
   isNewApply,
@@ -18,29 +22,29 @@ function UserConsultationStatus({
   return (
     <section className="mt-8  flex h-auto w-[380px] flex-col justify-between rounded-lg bg-white px-6 py-6 shadow-xl ">
       <div>
-        <h2 className="text-lg mb-2 border-b-1 border-background-disabled py-2 font-bold ">상담현황</h2>
+        <h2 className="py-2 mb-2 text-lg font-bold border-b-1 border-background-disabled ">상담현황</h2>
       </div>
-      <div className="mt-2 flex justify-between">
-        <div className="flex h-[68] w-[104px] flex-col items-center rounded-md border-1 border-b-secondary-heavy bg-white py-[14px] text-center  text-sm font-bold text-secondary-heavy">
-          <div className="relative h-8 w-8 text-xl">
-            {isNewApply && <div className="absolute right-0 top-0 h-2 w-2 rounded-xl bg-status-alert"></div>}
+      <div className="flex justify-between mt-2">
+        <div className={boxStyle}>
+          <div className="relative w-8 h-8 text-xl">
+            {isNewApply && <div className="absolute top-0 right-0 w-2 h-2 rounded-xl bg-status-alert"></div>}
             {applyCount}
           </div>
           예약신청
         </div>
-        <div className="flex h-[68] w-[104px] flex-col items-center rounded-md border-1 border-b-secondary-heavy bg-white py-[14px]  text-center text-sm font-bold text-secondary-heavy">
-          <div className="relative h-8 w-8 text-xl">
-            {isNewConfirm && <div className="absolute right-0 top-0 h-2 w-2 rounded-xl bg-status-alert"></div>}
+        <div className={boxStyle}>
+          <div className="relative w-8 h-8 text-xl">
+            {isNewConfirm && <div className="absolute top-0 right-0 w-2 h-2 rounded-xl bg-status-alert"></div>}
             {confirmCount}
           </div>
-          예약신청
+          예약확정
         </div>
-        <div className="flex h-[68] w-[104px] flex-col items-center rounded-md border-1 border-b-secondary-heavy bg-white py-[14px]  text-center text-sm font-bold text-secondary-heavy">
-          <div className="relative h-8 w-8 text-xl">
-            {isNewComplete && <div className="absolute right-0 top-0 h-2 w-2 rounded-xl bg-status-alert"></div>}
+        <div className={boxStyle}>
+          <div className="relative w-8 h-8 text-xl">
+            {isNewComplete && <div className="absolute top-0 right-0 w-2 h-2 rounded-xl bg-status-alert"></div>}
             {completeCount}
           </div>
-          예약신청
+          상담완료
         </div>
       </div>
     </section>

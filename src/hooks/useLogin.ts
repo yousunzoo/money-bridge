6813403a1fe0de3dc44e70f003ccb8efd1f32706
LeeByoungWook <lbw3973@naver.com>
2023-06-16 02:37:47 +0900

    Feat: login hook 제작

---
+++ src/hooks/useLogin.ts
@@ -0,0 +1,35 @@
+import { userLogin } from "@/app/apis/services/auth";
+import { useMutation } from "@tanstack/react-query";
+import { AxiosError } from "axios";
+import { useRouter } from "next/navigation";
+
+interface IResponseLogin {
+  status: number;
+  msg: string;
+  data: {
+    id: number;
+    code: string | null;
+  };
+}
+
+export const useLogin = (setNextStep: ((value: React.SetStateAction<boolean>) => void) | undefined) => {
+  const router = useRouter();
+
+  const { mutate } = useMutation(userLogin, {
+    onSuccess: (data: IResponseLogin) => {
+      console.log(data.data);
+      if (setNextStep) {
+        if (data.data.code) {
+          setNextStep(true);
+        }
+      } else {
+        router.push("/");
+      }
+    },
+    onError: (err: AxiosError) => {
+      console.log(err);
+    },
+  });
+
+  return mutate;
+};

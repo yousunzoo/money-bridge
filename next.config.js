030ba0f3421acba6c7ce186a36bdafd2acc8e3f1
yousunzoo <dpfvmffjqm@gmail.com>
2023-06-16 02:33:09 +0900

    Feat: /pblist로 접근 시 /pblist?speciality=all로 리다이렉트

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/pblist/",
+        destination: "/pblist?speciality=ALL",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig;

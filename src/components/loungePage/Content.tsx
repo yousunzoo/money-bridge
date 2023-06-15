fcb07ff64399310bd4d8283d1a6246e3d783fa05
hyeon17 <janghyeon8725@naver.com>
2023-06-16 05:52:01 +0900

    Design: 라운지 페이지 UI 추가

--- src/components/loungePage/Content.tsx
+++ src/components/loungePage/Content.tsx
@@ -1,14 +1,14 @@
-"use client";
 import React, { useState } from "react";
 import ContentCardList from "@/components/common/Card/CardList/ContentCardList";
 import Link from "next/link";
 import MainCarousel from "@/components/common/Carousel/MainCarousel";
 import "@/styles/carousel.css";
+import "@/styles/lounge.css";
 
-function Content({ NewAndHot, All }: any) {
-  const [all, setAll] = useState(All.data.list.slice(0, 2));
-  const newData = NewAndHot.data.list.slice(0, 2);
-  const hotData = NewAndHot.data.list.slice(2, 4);
+function Content({ NewAndHot,All }: any) {
+  const [all, setAll] = useState(All?.data?.list.slice(0, 2));
+  const [newData, setNewData] = useState(NewAndHot?.data?.list.slice(0, 2));
+  const [hotData, setHotData] = useState(NewAndHot?.data?.list.slice(2, 4));
 
   const getAllContent = () => {
     // setAll(All);
@@ -19,47 +19,77 @@ function Content({ NewAndHot, All }: any) {
   return (
     <div className="flex flex-col">
       <div>
-        <div className="flex">
-          <div>따끈따끈한 최신 콘텐츠 부터 읽어보세요</div>
-          <Link href="/new">더보기</Link>
+        <div className="header">
+          <div className="section">
+            따끈따끈한 최신 콘텐츠 부터
+            <br />
+            읽어보세요
+          </div>
+          <Link href="/new" className="more flex-3">
+            더보기
+          </Link>
         </div>
         <div>
           <ContentCardList props={newData} />
         </div>
       </div>
       <div>
-        <div className="flex">
-          <div>지금 가장 핫한 인기 콘텐츠</div>
-          <Link href="/hot">더보기</Link>
+        <div className="header">
+          <div className="section">
+            지금 가장 핫한
+            <br />
+            인기 콘텐츠
+          </div>
+          <Link href="/hot" className="more flex-3">
+            더보기
+          </Link>
         </div>
         <div>
           <ContentCardList props={hotData} />
         </div>
       </div>
-      <MainCarousel className="bg-black text-white">
-        <div className="pb_banner">
-          <div>경험 많은 프라이빗 뱅커의 포트폴리오가 궁금하신가요?</div>
-          <Link href="/findPb/list" className="underline hover:text-white hover:underline">
+      <MainCarousel className="my-11 h-[235px] text-white">
+        <div className="pb_banner bg-primary-light">
+          <div className="title">
+            경험 많은 프라이빗 뱅커를
+            <br />
+            만나보세요
+          </div>
+          <Link href="/findPb/list" className="link">
             PB 포트폴리오 보러가기
           </Link>
         </div>
-        <div className="pb_banner">
-          <div>가까운 거리의 PB를 바로 만나보세요</div>
-          <Link href="/findPb/list" className="underline hover:text-white hover:underline">
+        <div className="pb_banner bg-secondary-heavy">
+          <div className="title">
+            가까운 거리의 PB를
+            <br />
+            바로 만나보세요
+          </div>
+          <Link href="/findPb/list" className="link">
             가까운 거리의 PB 찾기
           </Link>
         </div>
-        <div className="pb_banner">
-          <div>선호하는 증권사별 PB를 찾아보세요</div>
-          <Link href="/findPb/list" className="underline hover:text-white hover:underline">
+        <div className="pb_banner bg-secondary-light">
+          <div className="title">
+            선호하는
+            <br />
+            증권사별 PB를 찾아보세요
+          </div>
+          <Link href="/findPb/list" className="link">
             증권사별 PB 보러가기
           </Link>
         </div>
       </MainCarousel>
       <div>
-        <div className="flex">
-          <div>머니 브릿지의 모든 콘텐츠를 한눈에 보세요</div>
-          <button onClick={getAllContent}>더보기</button>
+        <div className="header">
+          <div className="section">
+            머니 브릿지의 모든 콘텐츠를
+            <br />
+            한눈에 보세요
+          </div>
+          <button onClick={getAllContent} className="more flex-3">
+            더보기
+          </button>
         </div>
         <div>
           <ContentCardList props={all} />

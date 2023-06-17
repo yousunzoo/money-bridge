d1459922c5012abbd19ee5a6b6f70ec1ba092e91
hyeon17 <janghyeon8725@naver.com>
2023-06-17 21:39:52 +0900

    Design: 북마크 UI 작업

--- src/app/bookmark/page.tsx
+++ src/app/bookmark/page.tsx
@@ -1,16 +1,34 @@
-import React from 'react'
+"use client";
+import React from "react";
 import Link from "next/link";
 import TopNav from "@/components/common/TopNav";
+import { usePathname } from "next/navigation";
 
 function BookMark() {
+  const pathname = usePathname();
+
   return (
-    <>
-      <TopNav title="북마크" hasBack={true} />
-      <div>
-        <Link href="/bookmark/content">콘텐츠</Link>
-        <Link href="/bookmark/pb">PB</Link>
+    <div className="mb-[24px]">
+      <TopNav title="북마크 목록" hasBack={true} />
+      <div className="mx-[-16px] mb-6 flex h-[52px] items-center border-[1px] border-solid border-primary-normal text-base font-bold">
+        <Link
+          href="/bookmark/content"
+          className={`flex h-full w-full items-center justify-center pl-[8px] ${
+            pathname === "/bookmark/content" ? "bg-primary-normal text-white" : ""
+          }`}
+        >
+          콘텐츠
+        </Link>
+        <Link
+          href="/bookmark/pb"
+          className={`flex h-full w-full items-center justify-center pr-[8px] ${
+            pathname === "/bookmark/pb" ? "bg-primary-normal text-white" : ""
+          }`}
+        >
+          PB
+        </Link>
       </div>
-    </>
+    </div>
   );
 }
 
--- src/app/bookmark/pb/page.tsx
+++ src/app/bookmark/pb/page.tsx
@@ -1,14 +1,23 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import PbCardList from "@/components/common/Card/CardList/PbCardList";
 import BookMark from "@/app/bookmark/page";
-import PbData from "@/mocks/hyeon17/Common/pbList.json"
+import PbData from "@/mocks/hyeon17/Common/pbList.json";
+import { useRoleStore } from "@/store/roleStore";
 
 function PbBookMark() {
+  const userData = useRoleStore();
+  const [role, setRole] = useState("");
+
+  useEffect(() => {
+    setRole(userData.user.role);
+  }, [userData]);
+
   return (
-    <>
+    <div className="mb-10">
       <BookMark />
-      {PbData ? <PbCardList props={PbData} /> : <div>북마크 한 콘텐츠 없음</div>}
-    </>
+      {PbData ? <PbCardList props={PbData} role={role} /> : <div>북마크 한 콘텐츠 없음</div>}
+    </div>
   );
 }
 

ee2e3bf8ac535d0ea1cc2492684d7f7f8a2dfe2e
yousunzoo <dpfvmffjqm@gmail.com>
2023-06-18 00:36:25 +0900

    Feat: 유저 상담 현황 누르면 나의 상담 페이지로 이동

--- src/components/myPage/MyReservationStatus.tsx
+++ src/components/myPage/MyReservationStatus.tsx
@@ -1,4 +1,5 @@
 import { IMyReservationStatusProps } from "@/types/my";
+import Link from "next/link";
 
 const LI_STYLE = "w-[31%] text-center py-3 shadow-md font-bold border-1 border-secondary-heavy rounded-md";
 function MyReservationStatus({ reservationCount }: IMyReservationStatusProps) {
@@ -6,20 +7,22 @@ function MyReservationStatus({ reservationCount }: IMyReservationStatusProps) {
   return (
     <section className="mb-10 rounded-lg bg-white p-5 shadow-md">
       <h3 className="mb-4 border-b-1 border-background-normal pb-3 font-bold">나의 예약</h3>
-      <ul className="flex justify-between">
-        <li className={LI_STYLE}>
-          <p className="mb-3 text-[18px]">{apply}</p>
-          <p className="text-tiny">예약 신청</p>
-        </li>
-        <li className={LI_STYLE}>
-          <p className="mb-3 text-[18px]">{confirm}</p>
-          <p className="text-tiny">예약 확정</p>
-        </li>
-        <li className={LI_STYLE}>
-          <p className="mb-3 text-[18px]">{complete}</p>
-          <p className="text-tiny">상담 완료</p>
-        </li>
-      </ul>
+      <Link href="/myCounseling">
+        <ul className="flex justify-between">
+          <li className={LI_STYLE}>
+            <p className="mb-3 text-[18px]">{apply}</p>
+            <p className="text-tiny">예약 신청</p>
+          </li>
+          <li className={LI_STYLE}>
+            <p className="mb-3 text-[18px]">{confirm}</p>
+            <p className="text-tiny">예약 확정</p>
+          </li>
+          <li className={LI_STYLE}>
+            <p className="mb-3 text-[18px]">{complete}</p>
+            <p className="text-tiny">상담 완료</p>
+          </li>
+        </ul>
+      </Link>
     </section>
   );
 }

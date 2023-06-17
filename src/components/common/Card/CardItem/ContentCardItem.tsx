a0c171869e67353eba613cd0a8837c772ee710f1
hyeon17 <janghyeon8725@naver.com>
2023-06-17 19:18:24 +0900

    Design: 콘텐츠 카드 스타일 작업

--- src/components/common/Card/CardItem/ContentCardItem.tsx
+++ src/components/common/Card/CardItem/ContentCardItem.tsx
@@ -5,7 +5,7 @@ import { useRouter } from "next/navigation";
 import bookmark from "/public/assets/images/icon/pbcontent_bookmark.svg";
 import bookmark_filled from "/public/assets/images/icon/pbcontent_bookmark_filled.svg";
 
-function ContentCardItem({ item, user }: any) {
+function ContentCardItem({ item, role }: any) {
   const router = useRouter();
   const [isBookmark, setIsBookmark] = useState(false);
   const bookMark = (event: any) => {
@@ -27,20 +27,20 @@ function ContentCardItem({ item, user }: any) {
             </div>
             <div className="text-2xl font-bold">{item.title}</div>
           </div>
-          {user && (
+          {role && (
             <button onClick={bookMark} className="flex-3 flex w-12 items-center justify-center">
               {isBookmark ? <Image src={bookmark_filled} alt="북마크 해제" /> : <Image src={bookmark} alt="북마크" />}
             </button>
           )}
         </div>
-        <div className="mt-[66px] flex">
-          <div className="flex flex-col">
-            <div>
-              {item.pbName}/{item.career}
+        <div className="mt-20 flex">
+          <div className="flex flex-col text-[10px] h-[31px]">
+            <div className="flex">
+              <p className="font-bold">{item.pbName}PB</p>&nbsp;| {item.career}년차
             </div>
             <div>{item.msg}</div>
           </div>
-          {/* <Image src={item.companyLogo} alt="증권사로고" width={50} height={50} /> */}
+          <Image src={item.companyLogo} alt="증권사로고" className="w-[114px] h-[26px] ml-[45px]" width={0} height={0} />
         </div>
       </div>
     </li>

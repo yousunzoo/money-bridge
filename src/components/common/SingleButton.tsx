d227854d8f486cf10d86aa707fad181bf62959c4
kjun <kjun_all@naver.com>
2023-06-18 20:44:24 +0900

    Feat: 버튼 공통 컴포넌트

---
+++ src/components/common/SingleButton.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+interface ButtonProps {
+  title: string;
+  role: string;
+  ClickFunc: () => void;
+}
+
+function SingleButton({ title, role, ClickFunc }: ButtonProps) {
+  return (
+    <button
+      onClick={ClickFunc}
+      className={` ${
+        role === "USER" ? "bg-secondary-heavy" : "bg-primary-normal"
+      } h-14 w-full items-end rounded-md text-base font-bold text-white`}
+    >
+      {title}
+    </button>
+  );
+}
+
+export default SingleButton;

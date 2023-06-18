429301aecbfd3872d68c42aebcee3686daf08ec2
yousunzoo <dpfvmffjqm@gmail.com>
2023-06-18 14:39:20 +0900

    Feat: 차트 구현에 필요한 라이브러리 설치, 투자성향별 기대수익 세팅 해당 라이브러리가 리액트와 호환되지 않는 에러 발생, 디자이너와 논의 후 차트 재구현

--- src/components/myPage/propensityPage/PropensityChart.tsx
+++ src/components/myPage/propensityPage/PropensityChart.tsx
@@ -1,7 +1,29 @@
-import React from "react";
-
+import { Bar, ComposedChart, Line, Tooltip, XAxis, YAxis } from "recharts";
+import { propensityChart } from "@/constants/propensityList";
+import { useEffect, useState } from "react";
 function PropensityChart() {
-  return <div>PropensityChart</div>;
+  const [hydrated, setHydrated] = useState(false);
+  useEffect(() => {
+    setHydrated(true);
+  }, []);
+
+  if (!hydrated) {
+    return null;
+  }
+  return (
+    <section className="mb-10 w-full rounded-lg bg-white p-4 shadow-md">
+      <h3 className="mb-4 border-b-1 border-dashed border-background-normal pb-4 text-2xl font-bold text-primary-normal">
+        투자성향 별 기대 수익
+      </h3>
+      <ComposedChart width={400} height={350} className="-mx-8 w-full" data={propensityChart}>
+        <XAxis dataKey="name" scale="auto" width={20} />
+        <YAxis />
+        <Tooltip />
+        <Bar dataKey="volatility" barSize={20} fill="#EBEBEB" />
+        {/* <Line type="monotone" dataKey="volatility" stroke="#153445" /> */}
+      </ComposedChart>
+    </section>
+  );
 }
 
 export default PropensityChart;

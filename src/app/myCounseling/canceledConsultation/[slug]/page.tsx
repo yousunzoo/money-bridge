3dbc077de0392e5f392f7c274bdb374278455f82
kjun <kjun_all@naver.com>
2023-06-19 17:36:33 +0900

    Feat: 투자자 취소된 상담 페이지 로직 수정 및 구현

--- src/app/myCounseling/canceledConsultation/[slug]/page.tsx
+++ src/app/myCounseling/canceledConsultation/[slug]/page.tsx
@@ -4,7 +4,10 @@ import TopNav from "@/components/common/TopNav";
 import React from "react";
 import res from "../../../../mocks/kjun/managementReservations.json";
 import ConsultingHistoryCard from "../../../../components/common/ConsultingHistoryCard";
-import DoubleButton from "@/components/consultationPage/DoubleButton";
+import DoubleButton from "@/components/common/DoubleButton";
+import ConsultationScheduleSection from "@/components/common/ConsultationScheduleSection";
+import ConsultationLocationSection from "@/components/common/ConsultationLocationSection";
+import ConsultationNoteSection from "@/components/common/ConsultationNoteSection";
 
 interface Props {
   params: {
@@ -49,9 +52,6 @@ function CanceledConsultationPage({ params }: Props) {
   const role: string = "USER";
   const completionPhrase1 = "";
   const completionPhrase2 = "";
-  const onClickhandler = () => {
-    console.log("click");
-  };
 
   // 데이터 time 유무에 따라 상담일정, 희망일정 다르게
   const historyCard = {
@@ -69,24 +69,40 @@ function CanceledConsultationPage({ params }: Props) {
     completionPhrase2,
   };
 
+  const scheduleSectionProps = { candidateTime1, candidateTime2, role, time };
+  const locationSectionProps = { type, role, location, locationAddress };
+  const noteSectionProps = { role, goal, question };
+
+  const consultationRequestHandler = () => {
+    console.log("상담 다시 신청하기");
+  };
+
+  const checkClickHandler = () => {
+    console.log("확인");
+  };
+
   return (
     <div>
       <TopNav title="취소된 상담" hasBack={true} />
-      <div className={role === "USER" ? "user_top_Phrase" : "pb_top_Phrase"}>
+      <div className="user_top_Phrase">
         <span className="text-white ">취소된 상담입니다.</span>
       </div>
-      <UserReservationItem buttonName="PB 정보" onClickhandler={onClickhandler} isRole={"pb"}>
+      <UserReservationItem buttonName="PB 정보" href={"/"} isRole={"pb"}>
         <p className="font-bold">{name} PB</p>
         <p className="text-xs ">{"취소된 상담"}</p>
       </UserReservationItem>
 
-      <ConsultingHistoryCard {...historyCard} />
+      <section className="mt-6 w-full rounded-md bg-white p-4 pb-6 text-xs">
+        <ConsultationScheduleSection {...scheduleSectionProps} />
+        <ConsultationLocationSection {...locationSectionProps} />
+        <ConsultationNoteSection {...noteSectionProps} />
+      </section>
       <DoubleButton
         reviewCheck={reviewCheck}
         firstTitle={"상담 다시 신청하기"}
-        secondTitle={"후기 작성"}
-        firstClickFunc={onClickhandler}
-        secondClickFunc={onClickhandler}
+        secondTitle={"확인"}
+        firstClickFunc={consultationRequestHandler}
+        secondClickFunc={checkClickHandler}
         role={"USER"}
       />
     </div>

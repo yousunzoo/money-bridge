0a857f9e73c3053a164ad1798efe6a7f324652c2
LeeByoungWook <lbw3973@naver.com>
2023-06-19 16:00:44 +0900

    Test: 컴포넌트가 수정됨에 따른 다른 페이지에서 컴포넌트 주석

--- src/components/findPasswordPage/SelectInformation.tsx
+++ src/components/findPasswordPage/SelectInformation.tsx
@@ -1,14 +1,14 @@
 import React from "react";
-import InformationCheck from "../findEmailPage/InformationCheck";
 import { usePathname, useRouter } from "next/navigation";
+import InformationItem from "../findEmailPage/InformationItem";
 
 function SelectInformation() {
   const router = useRouter();
   const pathName = usePathname();
   return (
     <>
-      <p className="mb-10 mt-14 text-xl font-bold leading-7">해당하는 정보를 선택해주세요.</p>
-      <InformationCheck />
+      <p className="mb-10 mt-14 text-xl font-bold leading-7">회원 정보를 확인해주세요.</p>
+      {/* <InformationItem /> */}
       <button
         className="mb-24 mt-[266px] h-14 w-full rounded-[8px] bg-primary-normal text-xl font-bold leading-7 text-white"
         onClick={() => router.push(`/findPassword/${pathName.split("/")[2]}/resetPassword`)}

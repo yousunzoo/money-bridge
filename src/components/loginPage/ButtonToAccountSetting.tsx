ce66c21f981be220c4a586600ae1b3b971ccf2d2
LeeByoungWook <lbw3973@naver.com>
2023-06-19 15:41:51 +0900

    Feat: 이메일 찾기 경로 변경

--- src/components/loginPage/ButtonToAccountSetting.tsx
+++ src/components/loginPage/ButtonToAccountSetting.tsx
@@ -4,11 +4,12 @@ import React from "react";
 
 function ButtonToFind() {
   const pathName = usePathname();
+  const type = pathName.split("/")[2];
   return (
     <div className="mb-[60px] mt-[140px] flex h-5 items-center justify-center gap-4 text-sm leading-5 text-gray-heavy underline">
-      <Link href={`/join/${pathName.split("/")[2]}/email`}>회원가입</Link>
-      <Link href="/findEmail">이메일 찾기</Link>
-      <Link href={`/findPassword/${pathName.split("/")[2]}/enterInformation`}>비밀번호 찾기</Link>
+      <Link href={`/join/${type}/email`}>회원가입</Link>
+      <Link href={`/findEmail/${type}/enterInformation`}>이메일 찾기</Link>
+      <Link href={`/findPassword/${type}/enterInformation`}>비밀번호 찾기</Link>
     </div>
   );
 }

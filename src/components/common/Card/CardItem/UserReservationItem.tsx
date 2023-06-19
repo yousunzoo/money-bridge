dd6c2b1e782b77b1ca2d26162beeca9b21f95fb3
kjun <kjun_all@naver.com>
2023-06-19 16:42:00 +0900

    Feat: Button -> Link태그 props 변경

--- src/components/common/Card/CardItem/UserReservationItem.tsx
+++ src/components/common/Card/CardItem/UserReservationItem.tsx
@@ -3,16 +3,17 @@
 import React, { ReactNode } from "react";
 import DefaultProfile from "/public/assets/images/profile.svg";
 import Image from "next/image";
+import Link from "next/link";
 
 interface UserReservationItemProps {
   children: ReactNode;
   buttonName: string;
-  onClickhandler: () => void;
+  href: string;
   isRole: string;
   profileImage?: string;
 }
 
-function UserReservationItem({ children, buttonName, onClickhandler, isRole, profileImage }: UserReservationItemProps) {
+function UserReservationItem({ children, buttonName, href, isRole, profileImage }: UserReservationItemProps) {
   return (
     <li className="flex h-[90px] w-full justify-between overflow-hidden rounded-lg bg-white pl-4 shadow-md">
       <div className="flex items-center">
@@ -24,18 +25,18 @@ function UserReservationItem({ children, buttonName, onClickhandler, isRole, pro
           className=" max-w-[70px]"
         />
       </div>
-      <div className="flex flex-col justify-center px-4 py-3 rounded-lg ">{children}</div>
+      <div className="flex flex-col justify-center rounded-lg px-4 py-3 ">{children}</div>
 
-      <button
+      <Link
+        href={href}
         className={
           isRole === "USER"
-            ? "text-lg ml-auto h-auto w-[100px] justify-end rounded-r-lg bg-secondary-heavy text-sm font-bold text-white"
-            : "text-lg ml-auto h-auto w-[100px] justify-end rounded-r-lg bg-primary-normal text-sm font-bold text-white"
+            ? "text-lg ml-auto flex h-auto w-[100px] items-center justify-center rounded-r-lg bg-secondary-heavy text-sm font-bold text-white"
+            : "text-lg ml-auto flex h-auto w-[100px] items-center justify-center rounded-r-lg bg-primary-normal text-sm font-bold text-white"
         }
-        onClick={onClickhandler}
       >
-        {buttonName}
-      </button>
+        <span>{buttonName}</span>
+      </Link>
     </li>
   );
 }

f09b2acc6ab2997e69fb75b67368f5f49310cc7c
LeeByoungWook <lbw3973@naver.com>
2023-06-21 03:43:11 +0900

    Feat: 회원가입 슬러그 분리로 인한 Link경로 변경

--- src/components/loginPage/ButtonToAccountSetting.tsx
+++ src/components/loginPage/ButtonToAccountSetting.tsx
@@ -7,7 +7,7 @@ function ButtonToFind() {
   const type = pathName.split("/")[2];
   return (
     <div className="mb-[60px] mt-[140px] flex h-5 items-center justify-center gap-4 text-sm leading-5 text-gray-heavy underline">
-      <Link href={`/join/${type}/email`}>회원가입</Link>
+      <Link href={`/join/${type}/${type === "user" ? "email" : "pbJoinGuide"}`}>회원가입</Link>
       <Link href={`/findEmail/${type}/enterInformation`}>이메일 찾기</Link>
       <Link href={`/findPassword/${type}/enterInformation`}>비밀번호 찾기</Link>
     </div>

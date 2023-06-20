87b25d1c819cb04a3a53873b8901565327e40c62
LeeByoungWook <lbw3973@naver.com>
2023-06-20 13:22:00 +0900

    Feat: 비밀번호 찾기 모달 추가

--- src/hooks/usePasswordAuthentication.ts
+++ src/hooks/usePasswordAuthentication.ts
@@ -2,19 +2,25 @@ import { passwordAuthentication } from "@/app/apis/services/auth";
 import { useAuthenticationStore } from "@/store/authenticationStore";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { AxiosError } from "axios";
-import { useRouter } from "next/navigation";
+import { useState } from "react";
 
-export const usePasswordAuthentication = () => {
-  const router = useRouter();
+export const usePasswordAuthentication = (
+  setModalContent: React.Dispatch<React.SetStateAction<boolean>>,
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>,
+) => {
+  // const [isOpen ,setIsOpen] = useState(false)
   const queryClient = useQueryClient();
   const { setCode } = useAuthenticationStore();
 
   const { mutate } = useMutation(passwordAuthentication, {
     onSuccess: data => {
       console.log(data);
-      setCode(data.data.code);
       queryClient.setQueryData(["findPassword"], data);
-      router.push(`/findPassword/${data.data.role.toLowerCase()}/authentication`);
+      setModalContent(data.data.id !== null);
+      setIsOpen(true);
+      if (data.data.id) {
+        setCode(data.data.code);
+      }
     },
     onError: (err: AxiosError) => {
       console.log(err);

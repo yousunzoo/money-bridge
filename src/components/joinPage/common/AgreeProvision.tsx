23b07ebc4e58ae1d38f4bff22c5344575434f6c5
LeeByoungWook <lbw3973@naver.com>
2023-06-22 03:37:41 +0900

    Feat: 약관 컴포넌트 분리 및 약관 상세보기 컴포넌트 구현

--- src/components/joinPage/common/AgreeProvision.tsx
+++ src/components/joinPage/common/AgreeProvision.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { MouseEvent, useState } from "react";
 import checkProvision from "/public/assets/images/checkProvision.svg";
 import uncheckProvision from "/public/assets/images/uncheckProvision.svg";
 import openProvision from "/public/assets/images/openProvision.svg";
@@ -7,25 +7,51 @@ import { usePathname, useRouter } from "next/navigation";
 import { useJoinStore } from "@/store/joinStore";
 import { useUserJoin } from "@/hooks/useUserJoin";
 import { usePBJoin } from "@/hooks/usePBJoin";
+import DetailProvision from "./DetailProvision";
+import ProvisionContent from "./ProvisionContent";
 
-const required = [
-  { id: 0, title: "머니브릿지 이용약관" },
+interface IProvision {
+  id: number;
+  title: string;
+}
+
+export const userRequired: IProvision[] = [
+  { id: 0, title: "시스메틱 이용약관 동의" },
   { id: 1, title: "개인정보 취급방침 동의" },
   { id: 2, title: "개인정보 제 3자 제공 동의" },
 ];
-const optional = [
-  { id: required.length, title: "위치기반 서비스 이용 동의" },
-  { id: required.length + 1, title: "포트폴리오 관련 정보 수신 동의" },
-  { id: required.length + 2, title: "마케팅 및 정보성 알림 수신 동의" },
+
+export const pbRequired: IProvision[] = [
+  { id: 0, title: "시스메틱 이용약관 동의" },
+  { id: 1, title: "개인정보 취급방침 동의" },
+  { id: 2, title: "개인정보 제 3자 제공 동의" },
+];
+const optional: IProvision[] = [
+  // { id: required.length, title: "위치기반 서비스 이용 동의" },
+  // { id: required.length + 1, title: "포트폴리오 관련 정보 수신 동의" },
+  // { id: required.length + 2, title: "마케팅 및 정보성 알림 수신 동의" },
 ];
 
 function AgreeProvision() {
   const router = useRouter();
   const pathName = usePathname();
+  const required = pathName.split("/")[2] === "user" ? userRequired : pbRequired;
   const { informations, setInformations } = useJoinStore();
   const userJoin = useUserJoin();
   const pbJoin = usePBJoin();
   const [isChecked, setIsChecked] = useState<{ [key: number]: boolean }>(required.concat(optional).map(() => false));
+  const [isOpen, setIsOpen] = useState(false);
+  const [provisionId, setProvisionId] = useState<number>(0);
+
+  const handleOpenProvision = (e: MouseEvent<HTMLElement>) => {
+    setIsOpen(true);
+    const target = e.target as HTMLButtonElement;
+    setProvisionId(Number(target.id));
+  };
+
+  const handleCloseProvision = () => {
+    setIsOpen(false);
+  };
 
   const handleCheckboxClick = (key: number) => {
     setIsChecked({ ...isChecked, [key]: !isChecked[key] });
@@ -69,63 +95,54 @@ function AgreeProvision() {
     setInformations("agreements", data);
     const joinType = pathName.split("/")[2];
     joinType === "user" ? userJoin(informations) : pbJoin(informations);
-    router.push(`/join/${joinType}/complete`);
   };
 
   return (
     <>
-      <p className="my-14 text-xl font-bold leading-7">약관을 확인해 주세요</p>
-      <div className="border mb-6 rounded-sm border-1 border-button-inactive">
-        <div className="border relative flex h-14 w-full min-w-[358px] items-center gap-4 pl-6 pr-4">
-          <button className="flex h-6 w-6 items-center justify-center" onClick={handleAllClick}>
-            <Image src={isAllChecked() ? checkProvision : uncheckProvision} alt="check" />
+      {isOpen ? (
+        <DetailProvision handleCloseProvision={handleCloseProvision} provisionId={provisionId} />
+      ) : (
+        <>
+          <p className="my-14 text-xl font-bold leading-7">약관을 확인해 주세요</p>
+          <div className="border mb-6 rounded-sm border-1 border-button-inactive">
+            <div className="border relative flex h-14 w-full min-w-[358px] items-center gap-4 pl-6 pr-4">
+              <button className="flex h-6 w-6 items-center justify-center" onClick={handleAllClick}>
+                <Image src={isAllChecked() ? checkProvision : uncheckProvision} alt="check" width={18} height={24} />
+              </button>
+              <span className="text-xl font-bold leading-7">약관 전체 동의</span>
+              <button className="absolute right-6 flex h-6 w-6 items-center justify-center">
+                <Image src={openProvision} alt="open" width={13} height={21} />
+              </button>
+            </div>
+          </div>
+          <div className="border mb-6 rounded-sm border-1 border-button-inactive">
+            {required.map(item => (
+              <div key={item.id}>
+                {ProvisionContent(item.id, item.title, handleCheckboxClick, isChecked[item.id], handleOpenProvision)}
+              </div>
+            ))}
+          </div>
+          {optional.length !== 0 && (
+            <div className="border mb-6 rounded-sm border-1 border-button-inactive">
+              {optional.map(item => (
+                <div key={item.id}>
+                  {ProvisionContent(item.id, item.title, handleCheckboxClick, isChecked[item.id], handleOpenProvision)}
+                </div>
+              ))}
+            </div>
+          )}
+          <button
+            className={`mb-[50px] mt-8 h-14 w-full rounded-[8px]  text-xl font-bold leading-7 ${
+              isNecessaryAllChecked() ? "bg-primary-normal text-white" : "bg-background-secondary text-gray-heavy"
+            }`}
+            onClick={handleSubmit}
+            disabled={!isNecessaryAllChecked()}
+          >
+            회원가입 완료
           </button>
-          <span className="text-xl font-bold leading-7">약관 전체 동의</span>
-          <button className="absolute right-6 flex h-6 w-6 items-center justify-center">
-            <Image src={openProvision} alt="open" />
-          </button>
-        </div>
-      </div>
-      <div className="border mb-6 rounded-sm border-1 border-button-inactive">
-        {required.map(item => (
-          <div key={item.id}>{provisionContent(item.id, item.title, handleCheckboxClick, isChecked[item.id])}</div>
-        ))}
-      </div>
-      <div className="border mb-6 rounded-sm border-1 border-button-inactive">
-        {optional.map(item => (
-          <div key={item.id}>{provisionContent(item.id, item.title, handleCheckboxClick, isChecked[item.id])}</div>
-        ))}
-      </div>
-      <button
-        className={`mb-[50px] mt-8 h-14 w-full rounded-[8px]  text-xl font-bold leading-7 ${
-          isNecessaryAllChecked() ? "bg-primary-normal text-white" : "bg-background-secondary text-gray-heavy"
-        }`}
-        onClick={handleSubmit}
-        disabled={!isNecessaryAllChecked()}
-      >
-        회원가입 완료
-      </button>
+        </>
+      )}
     </>
   );
 }
 export default AgreeProvision;
-
-const provisionContent = (key: number, subTitle: string, setIsChecked: (index: number) => void, isChecked: boolean) => {
-  const handleClick = () => {
-    setIsChecked(key);
-  };
-  return (
-    <div className={`border relative flex h-14 w-full min-w-[358px] items-center gap-4 pl-6 pr-4 `}>
-      <button className="flex h-6 w-6 items-center justify-center" onClick={handleClick}>
-        <Image src={isChecked ? checkProvision : uncheckProvision} alt="check" />
-      </button>
-      <span className="leading-6">
-        {key < required.length ? "[필수] " : "[선택] "}
-        {subTitle}
-      </span>
-      <button className="absolute right-6 flex h-6 w-6 items-center justify-center">
-        <Image src={openProvision} alt="open" />
-      </button>
-    </div>
-  );
-};

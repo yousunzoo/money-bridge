e5ac8fd7bae09b1f427712ef212e70c3b2ac0657
LeeByoungWook <lbw3973@naver.com>
2023-06-21 22:56:54 +0900

    Rename: 파일 이름 변경

--- src/components/loginPage/ButtonToAccountSetting.tsx
+++ src/components/loginPage/ButtonToAccountSetting.tsx
@@ -2,11 +2,11 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React from "react";
 
-function ButtonToFind() {
+function ButtonToAccountSetting() {
   const pathName = usePathname();
   const type = pathName.split("/")[2];
   return (
-    <div className="mb-[60px] mt-[140px] flex h-5 items-center justify-center gap-4 text-sm leading-5 text-gray-heavy underline">
+    <div className="mb-[60px] mt-[60px] flex h-5 items-center justify-center gap-4 text-sm leading-5 text-gray-heavy underline">
       <Link href={`/join/${type}/${type === "user" ? "email" : "pbJoinGuide"}`}>회원가입</Link>
       <Link href={`/findEmail/${type}/enterInformation`}>이메일 찾기</Link>
       <Link href={`/findPassword/${type}/enterInformation`}>비밀번호 찾기</Link>
@@ -14,4 +14,4 @@ function ButtonToFind() {
   );
 }
 
-export default ButtonToFind;
+export default ButtonToAccountSetting;

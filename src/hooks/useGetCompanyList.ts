2bfff10a44c647fd144ba8a85658b4dfd0263c92
LeeByoungWook <lbw3973@naver.com>
2023-06-21 22:58:58 +0900

    Feat: 증권사 불러오기 hook 구현

--- src/hooks/useGetCompanyList.ts
+++ src/hooks/useGetCompanyList.ts
@@ -1,8 +1,8 @@
 import { getCompanyList } from "@/app/apis/services/auth";
-import { ICompanyList } from "@/types/join";
+import { ICompanyNameList } from "@/types/join";
 import { useQuery } from "@tanstack/react-query";
 
 export const useGetCompanyList = () => {
-  const { data } = useQuery<any, unknown, ICompanyList, any>(["companyList"], getCompanyList);
+  const { data } = useQuery<any, unknown, ICompanyNameList, any>(["companyList"], getCompanyList);
   return { data };
 };

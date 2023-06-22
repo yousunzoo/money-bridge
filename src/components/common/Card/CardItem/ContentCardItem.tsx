d0ed44cf3c2ee2d010099b117242f2c617f09529
kjun <kjun_all@naver.com>
2023-06-23 00:03:05 +0900

    Style: 태그 위치 및 css 코드 수정

--- src/components/common/Card/CardItem/ContentCardItem.tsx
+++ src/components/common/Card/CardItem/ContentCardItem.tsx
@@ -20,17 +20,17 @@ function ContentCardItem({ item }: { item: any }) {
   };
 
   return (
-    <li className="h-56 bg-white cursor-pointer card" onClick={goTOLounge}>
-      <div className="px-[17px]">
-        <div className="flex mt-7">
-          <div className="flex-1">
-            <div className="mb-1 text-[8px]">
+    <li className="h-56 p-6 bg-white cursor-pointer card" onClick={goTOLounge}>
+      <div className="flex flex-col justify-between h-full">
+        <div className="flex justify-between">
+          <div className="flex flex-col ">
+            <div className="text-base ">
               {item.tag1}&nbsp;&nbsp;•&nbsp;&nbsp;{item.tag2}
             </div>
             <div className="text-2xl font-bold">{item.title}</div>
           </div>
           {userData.user.role && (
-            <button onClick={bookMark} className="flex items-center justify-center w-12 flex-3">
+            <button onClick={bookMark} className="flex p-2">
               {item.isBookmark ? (
                 <Image src={bookmark_filled} alt="북마크 해제" />
               ) : (
@@ -39,11 +39,10 @@ function ContentCardItem({ item }: { item: any }) {
             </button>
           )}
         </div>
-        <div className="flex mt-20">
-          <div className="flex h-[31px] flex-col text-[10px]">
-            <div className="flex">
-              <p className="font-bold">{item.pbName}PB</p>&nbsp;| {item.career}년차
-            </div>
+        <div className="flex justify-between">
+          <div>
+            <span className="font-bold">{item.pbName}PB&nbsp;&nbsp;</span>
+            <span>|&nbsp;&nbsp;{item.career}년차</span>
             <div>{item.msg}</div>
           </div>
           {/* <Image src={item.companyLogo} alt="증권사로고" className="w-[114px] h-[26px] ml-[45px]" width={0} height={0} /> */}

f49f95c9bcb0372ed205118f41f5d362a7e57716
kjun <kjun_all@naver.com>
2023-06-22 14:00:51 +0900

    Feat: GeoLocation 훅 추가

---
+++ src/hooks/useGeoLaction.tsx
@@ -0,0 +1,36 @@
+"use client";
+import { getLocationName } from "@/app/apis/services/location";
+import { useLocationStore } from "@/store/location";
+import { CoordinateProps, PositionProps } from "@/types/location";
+import { useEffect } from "react";
+
+export const useGeoLocation = () => {
+  const { locations, setLocation, setCoordinate } = useLocationStore();
+
+  useEffect(() => {
+    const { geolocation } = navigator;
+    if (!geolocation) {
+      alert("위치가 지원되지 않습니다.");
+      return;
+    }
+
+    function onGeoOkay(position: PositionProps) {
+      const { latitude, longitude } = position.coords;
+      if (latitude) {
+        setCoordinate({ latitude, longitude });
+      }
+    }
+
+    geolocation.getCurrentPosition(onGeoOkay);
+    if (!locations.location) {
+      geoLocationFunc({ ...locations.coordinate });
+    }
+  }, []);
+
+  const geoLocationFunc = async ({ latitude, longitude }: CoordinateProps) => {
+    const data = await getLocationName({ latitude, longitude });
+    setLocation(data);
+  };
+
+  return null;
+};

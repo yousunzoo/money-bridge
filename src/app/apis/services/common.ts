d53a8f79a5f1fe11d0a2aa53d4ba06a7bb4985be
kjun <kjun_all@naver.com>
2023-06-22 23:59:14 +0900

    Feat: axios API 코드 구현 - 메인 추천PB 가져오기 - 메인 컨텐츠 가져오기

--- src/app/apis/services/common.ts
+++ src/app/apis/services/common.ts
@@ -1,5 +1,7 @@
+import { CoordinateProps } from "@/types/location";
 import { instance } from "../axios";
 import { useQuery } from "@tanstack/react-query";
+import { PbListSectionPorps } from "@/types/main";
 
 export const useLoungeBoard = () => {
   const queryKey = "/lounge/board";
@@ -9,4 +11,27 @@ export const useLoungeBoard = () => {
     });
 
   return useQuery([queryKey], queryFn);
+};
+
+export const getSuggestionPB = async ({ latitude, longitude }: CoordinateProps): Promise<PbListSectionPorps[]> => {
+  try {
+    const res = await instance.get("/main/pb", {
+      params: {
+        latitude,
+        longitude,
+      },
+    });
+    return res.data.data.list;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getContents = async () => {
+  try {
+    const res = await instance.get("/main/board");
+    return res.data.data.list;
+  } catch (error) {
+    throw error;
+  }
 };

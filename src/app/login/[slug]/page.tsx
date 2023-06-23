3b1fdebf2dfad16eb867d94fbe6d04886f1ab0e8
LeeByoungWook <lbw3973@naver.com>
2023-06-24 01:55:08 +0900

    Feat: path에 따른 TopNav 타이틀 변경

--- src/app/login/[slug]/page.tsx
+++ src/app/login/[slug]/page.tsx
@@ -6,13 +6,19 @@ import KeepLoginButton from "@/components/loginPage/KeepLoginButton";
 import { InputFormType } from "@/constants/enum";
 import { useState } from "react";
 import AdminAuthentication from "@/components/loginPage/AdminAuthentication";
+import { usePathname } from "next/navigation";
 
 function Login() {
   const [nextStep, setNextStep] = useState(false);
+  const pathName = usePathname();
 
   return (
     <>
-      <TopNav title={`${nextStep ? "관리자 인증" : "로그인"}`} hasBack backGroundWhite />
+      <TopNav
+        title={`${nextStep ? "관리자 인증" : pathName.split("/")[2] === "user" ? "유저 로그인" : "PB 로그인"}`}
+        hasBack
+        backGroundWhite
+      />
       <div className="mt-10 text-black">
         {nextStep ? (
           <AdminAuthentication />

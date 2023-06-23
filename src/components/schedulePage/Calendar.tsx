66a2fdd39307f133d34c6de3aae9436c804f587b
kjun <kjun_all@naver.com>
2023-06-23 19:03:40 +0900

    Feat: 일정 내역(연, 월 변경시) 캘린더 로직 추가

--- src/components/schedulePage/Calendar.tsx
+++ src/components/schedulePage/Calendar.tsx
@@ -6,39 +6,42 @@ import type { Dayjs } from "dayjs";
 import type { CellRenderInfo } from "rc-picker/lib/interface";
 import dayjs from "dayjs";
 import { ManagementCalendarProps } from "@/types/schedule";
+import "dayjs/locale/ko";
+import locale from "antd/es/calendar/locale/ko_KR";
 
 dayjs.locale("ko");
 
-const ManagementCalendar: React.FC<ManagementCalendarProps> = ({ reservationList, setIsClickDay }) => {
-  const [scheduleData, setScheduleData] = useState(reservationList); // 초기 데이터로 상태 초기화
+const ManagementCalendar: React.FC<ManagementCalendarProps> = ({ reservationList, setIsClickDay, setClickDate }) => {
+  const [scheduleData, setScheduleData] = useState(reservationList || undefined); // 초기 데이터로 상태 초기화
   const [initialDateSelected, setInitialDateSelected] = useState(false); // 초기 날짜 선택 여부를 확인하는 상태, 오늘 날짜 선택
 
   useEffect(() => {
-    setScheduleData(reservationList);
-
-    if (!initialDateSelected && reservationList.length > 0) {
-      const today = dayjs().format("YYYY-MM-DD");
-      setIsClickDay(today);
-      setInitialDateSelected(true);
+    setScheduleData(reservationList!);
+    if (reservationList !== undefined) {
+      if (!initialDateSelected && reservationList.length > 0) {
+        const today = dayjs().format("YYYY-MM-DD");
+        setIsClickDay(today);
+        setInitialDateSelected(true);
+      }
     }
   }, []);
 
   const getListData = (value: Dayjs) => {
     const dateString = value.format("YYYY-MM-DD");
-    const filteredData = scheduleData.filter(item => item.day === dateString);
+    const filteredData = scheduleData?.filter(item => item.day === dateString);
 
     // 데이터에 따라 스케줄 상태를 반환
-    const listData = filteredData.map(item => {
+    const listData = filteredData?.map(item => {
       let status: BadgeProps["status"] = "default";
 
       switch (item.process) {
-        case "상담완료":
+        case "COMPLETE":
           status = "success";
           break;
-        case "예약확정":
+        case "CONFIRM":
           status = "warning";
           break;
-        case "신규예약":
+        case "APPLY":
           status = "error";
           break;
       }
@@ -58,8 +61,11 @@ const ManagementCalendar: React.FC<ManagementCalendarProps> = ({ reservationList
 
   const dateCellRender = (value: Dayjs) => {
     const listData = getListData(value);
-    const setListData = listData.filter((obj, index, self) => index === self.findIndex(item => item.type === obj.type));
-    return setListData.length ? (
+
+    const setListData = listData?.filter(
+      (obj, index, self) => index === self.findIndex(item => item.type === obj.type),
+    );
+    return setListData?.length ? (
       <ul className="events">
         {setListData.map((item, index) => (
           <li key={index}>
@@ -72,7 +78,7 @@ const ManagementCalendar: React.FC<ManagementCalendarProps> = ({ reservationList
 
   const disabledDate = (current: Dayjs) => {
     const listData = getListData(current);
-    return listData.length === 0;
+    return listData?.length === 0;
   };
 
   const cellRender = (current: Dayjs, info: CellRenderInfo<Dayjs>) => {
@@ -80,7 +86,26 @@ const ManagementCalendar: React.FC<ManagementCalendarProps> = ({ reservationList
     return info.originNode;
   };
 
-  return <Calendar cellRender={cellRender} onSelect={onSelect} disabledDate={disabledDate} />;
+  const handlePanelChange = (value: Dayjs, mode: string) => {
+    if (mode === "year" || mode === "month") {
+      const year = value.year();
+      const month = value.month() + 1;
+      setClickDate({
+        year: year,
+        month: month,
+      });
+    }
+  };
+
+  return (
+    <Calendar
+      locale={locale}
+      cellRender={cellRender}
+      onSelect={onSelect}
+      disabledDate={disabledDate}
+      onPanelChange={handlePanelChange}
+    />
+  );
 };
 
 export default ManagementCalendar;

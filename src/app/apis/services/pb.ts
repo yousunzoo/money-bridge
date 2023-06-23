d016fbda98ca7062a15197fec5eadb65697284ca
kjun <kjun_all@naver.com>
2023-06-23 18:55:00 +0900

    Feat: 월별/일별 예약 정보 조회 API 로직

--- src/app/apis/services/pb.ts
+++ src/app/apis/services/pb.ts
@@ -1,3 +1,4 @@
+import { ConsultationTimeCardProps } from "@/types/schedule";
 import { instance } from "../axios";
 
 export const getPBInfo = async () => {
@@ -7,4 +8,46 @@ export const getPBInfo = async () => {
   } catch (error: any) {
     throw new Error(error.response.data.status);
   }
+};
+
+interface GetScheduleInfoProps {
+  year?: number;
+  month?: number;
+}
+
+export const getScheduleInfo = async ({ year, month }: GetScheduleInfoProps) => {
+  try {
+    const res = await instance.get("/pb/reservation", {
+      params: {
+        year,
+        month,
+      },
+    });
+    return res.data.data;
+  } catch (error) {}
+};
+
+export const getConsultTime = async () => {
+  try {
+    const res = await instance.get("/pb/consultTime");
+
+    return res.data.data;
+  } catch (error) {}
+};
+
+export interface ConsultTimeProps {
+  consultStart: string;
+  consultEnd: string;
+  consultNotice: string;
+}
+export const updateConsultTime = async ({ consultStart, consultEnd, consultNotice }: ConsultationTimeCardProps) => {
+  try {
+    const res = await instance.post("/pb/consultTime", {
+      consultStart,
+      consultEnd,
+      consultNotice,
+    });
+
+    return res;
+  } catch (error) {}
 };

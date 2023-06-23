0e7767f46695b65a91ce3bef7da0266163d5bbc9
kjun <kjun_all@naver.com>
2023-06-23 19:07:01 +0900

    Design: 반응형 스타일 코드 추가

--- src/components/schedulePage/InfoModal.tsx
+++ src/components/schedulePage/InfoModal.tsx
@@ -10,14 +10,14 @@ const statusItems = [
 
 function InfoModal() {
   return (
-    <div className="absolute right-14 top-16 z-10 flex h-[140px] w-[280px] flex-col rounded-xl bg-white p-4 shadow-xl ">
-      <div className="flex h-auto w-full items-end justify-between">
+    <div className="absolute right-7 top-14 z-10 flex h-[140px] w-[280px] flex-col rounded-md bg-white p-4 shadow-md ">
+      <div className="flex items-end justify-between w-full h-auto">
         <div className="text-sm">
           <Image src={question} alt="question" width={20} height={20} className="mb-2" />
           <p className="text-sm font-bold ">컬러로 상담 현황을</p>
           <p className="text-sm font-bold ">한눈에 파악하세요.</p>
         </div>
-        <div className="flex h-[52px] w-[52px] flex-col items-center rounded-lg bg-white p-2 shadow-2xl">
+        <div className="flex h-[52px] w-[52px] flex-col items-center rounded-md bg-white p-2 shadow-md">
           <div className="font-bold">9</div>
           <ul className="flex gap-1">
             {statusItems.map(item => (
@@ -26,9 +26,9 @@ function InfoModal() {
           </ul>
         </div>
       </div>
-      <ul className="mt-3 flex gap-2">
+      <ul className="flex gap-2 mt-3">
         {statusItems.map(item => (
-          <li key={item.id} className={`w-[60px] rounded-full  p-2 text-center text-[10px] text-white ${item.color}`}>
+          <li key={item.id} className={`w-[60px] rounded-sm  p-2 text-center text-[10px] text-white ${item.color}`}>
             {item.text}
           </li>
         ))}

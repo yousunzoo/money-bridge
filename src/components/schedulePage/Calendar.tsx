ab56f610e180c413b80c3d84bdd28413bcb8ff86
kjun <kjun_all@naver.com>
2023-06-25 00:14:40 +0900

    Refactor: 캘린더 코드 리팩토링

--- src/components/schedulePage/Calendar.tsx
+++ src/components/schedulePage/Calendar.tsx
@@ -1,5 +1,4 @@
-"use client";
-import React, { useEffect, useState } from "react";
+import React, { memo, useCallback, useEffect, useState } from "react";
 import type { BadgeProps } from "antd";
 import { Badge, Calendar } from "antd";
 import type { Dayjs } from "dayjs";
@@ -11,46 +10,54 @@ import locale from "antd/es/calendar/locale/ko_KR";
 
 dayjs.locale("ko");
 
-const ManagementCalendar: React.FC<ManagementCalendarProps> = ({ reservationList, setIsClickDay, setClickDate }) => {
-  const [scheduleData, setScheduleData] = useState(reservationList || undefined); // 초기 데이터로 상태 초기화
+const ManagementCalendar: React.FC<ManagementCalendarProps> = React.memo(function ManagementCalendar({
+  reservationList,
+  setIsClickDay,
+  setClickDate,
+}) {
+  const [scheduleData, setScheduleData] = useState(reservationList); // 초기 데이터로 상태 초기화
   const [initialDateSelected, setInitialDateSelected] = useState(false); // 초기 날짜 선택 여부를 확인하는 상태, 오늘 날짜 선택
 
   useEffect(() => {
-    setScheduleData(reservationList!);
-    if (reservationList !== undefined) {
-      if (!initialDateSelected && reservationList.length > 0) {
-        const today = dayjs().format("YYYY-MM-DD");
-        setIsClickDay(today);
-        setInitialDateSelected(true);
-      }
+    if (reservationList === undefined) {
+      return; // reservationList가 undefined일 경우, useEffect를 종료하고 이후 로직을 실행하지 않음
     }
-  }, []);
 
-  const getListData = (value: Dayjs) => {
-    const dateString = value.format("YYYY-MM-DD");
-    const filteredData = scheduleData?.filter(item => item.day === dateString);
+    setScheduleData(reservationList);
+    if (!initialDateSelected && reservationList.length > 0) {
+      const today = dayjs().format("YYYY-MM-DD");
+      setIsClickDay(today);
+      setInitialDateSelected(true);
+    }
+  }, [reservationList]);
 
-    // 데이터에 따라 스케줄 상태를 반환
-    const listData = filteredData?.map(item => {
-      let status: BadgeProps["status"] = "default";
+  const getListData = useCallback(
+    (value: Dayjs) => {
+      const dateString = value.format("YYYY-MM-DD");
+      const filteredData = scheduleData?.filter(item => item.day === dateString);
+      if (filteredData !== undefined && filteredData.length > 0) {
+        const listData = filteredData?.map(item => {
+          let status: BadgeProps["status"] = "default";
 
-      switch (item.process) {
-        case "COMPLETE":
-          status = "success";
-          break;
-        case "CONFIRM":
-          status = "warning";
-          break;
-        case "APPLY":
-          status = "error";
-          break;
+          switch (item.process) {
+            case "COMPLETE":
+              status = "success";
+              break;
+            case "CONFIRM":
+              status = "warning";
+              break;
+            case "APPLY":
+              status = "error";
+              break;
+          }
+
+          return { type: status };
+        });
+        return listData;
       }
-
-      return { type: status };
-    });
-
-    return listData;
-  };
+    },
+    [scheduleData],
+  );
 
   const onSelect = (date: Dayjs | null) => {
     if (date) {
@@ -59,26 +66,32 @@ const ManagementCalendar: React.FC<ManagementCalendarProps> = ({ reservationList
     }
   };
 
-  const dateCellRender = (value: Dayjs) => {
-    const listData = getListData(value);
-
-    const setListData = listData?.filter(
-      (obj, index, self) => index === self.findIndex(item => item.type === obj.type),
-    );
-    return setListData?.length ? (
-      <ul className="events">
-        {setListData.map((item, index) => (
-          <li key={index}>
-            <Badge status={item.type as BadgeProps["status"]} />
-          </li>
-        ))}
-      </ul>
-    ) : null;
-  };
+  const dateCellRender = useCallback(
+    (value: Dayjs) => {
+      const listData = getListData(value);
+      if (listData === undefined) return;
+      const setListData = listData.filter(
+        (obj, index, self) => index === self.findIndex(item => item.type === obj.type),
+      );
+      return (
+        setListData.length && (
+          <ul className="events">
+            {setListData.map((item, index) => (
+              <li key={index}>
+                <Badge status={item.type as BadgeProps["status"]} />
+              </li>
+            ))}
+          </ul>
+        )
+      );
+    },
+    [scheduleData],
+  );
 
   const disabledDate = (current: Dayjs) => {
     const listData = getListData(current);
-    return listData?.length === 0;
+    if (listData === undefined || listData.length === 0) return true;
+    return false;
   };
 
   const cellRender = (current: Dayjs, info: CellRenderInfo<Dayjs>) => {
@@ -106,6 +119,6 @@ const ManagementCalendar: React.FC<ManagementCalendarProps> = ({ reservationList
       onPanelChange={handlePanelChange}
     />
   );
-};
+});
 
 export default ManagementCalendar;

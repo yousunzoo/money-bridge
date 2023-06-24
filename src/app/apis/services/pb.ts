aa61c092babd797b143a6b0451dd38e8fc1a1cda
kjun <kjun_all@naver.com>
2023-06-25 04:21:55 +0900

    Feat: 고객관리 페이지 상담현황, process별 리스트 API 로직

--- src/app/apis/services/pb.ts
+++ src/app/apis/services/pb.ts
@@ -1,3 +1,4 @@
+import { AxiosError } from "axios";
 import { instance } from "../axios";
 
 export const getPBInfo = async () => {
@@ -6,5 +7,27 @@ export const getPBInfo = async () => {
     return res.data;
   } catch (error: any) {
     throw new Error(error.response.data.status);
+  }
+};
+
+export const getConsultationStatus = async () => {
+  try {
+    const res = await instance.get("pb/management/recent");
+    return res.data.data;
+  } catch (error: any) {
+    throw new AxiosError(error.response.data);
+  }
+};
+
+export const getConsultationList = async (type: string) => {
+  try {
+    const res = await instance.get("pb/management/reservations", {
+      params: {
+        type,
+      },
+    });
+    return res.data.data.list;
+  } catch (error: any) {
+    throw new AxiosError(error.response.data);
   }
 };

df0c512eead5b47cf4f07169f72c23633d74ac00
kjun <kjun_all@naver.com>
2023-06-24 19:53:02 +0900

    Feat: 리액트쿼리 defaultOptions 설정

--- src/app/ReactQueryProvider.tsx
+++ src/app/ReactQueryProvider.tsx
@@ -1,14 +1,29 @@
 "use client";
 
+import ButtonModal from "@/components/common/ButtonModal";
+import useApiError from "@/hooks/useApiError";
 import { QueryClient, QueryClientProvider, Hydrate } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { PropsWithChildren, useState } from "react";
+import { getUserInfo } from "./apis/services/user";
 
 export default function ReactQueryProvider({ children }: PropsWithChildren) {
   const [queryClient] = useState(() => new QueryClient());
+  const { handleError, isOpen, modalContents, setIsOpen } = useApiError();
+
+  queryClient.setDefaultOptions({
+    queries: { onError: (error: any) => handleError(error) },
+    mutations: {
+      onError: (error: any) => {
+        handleError(error);
+      },
+    },
+  });
+
   return (
     <QueryClientProvider client={queryClient}>
       {children}
+      {isOpen && <ButtonModal modalContents={modalContents} isOpen={isOpen} setIsOpen={setIsOpen} />}
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );

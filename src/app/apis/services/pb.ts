a7e21d5fe80dd5b47f0c39ef5b2d2c1a81caca41
kjun <kjun_all@naver.com>
2023-06-26 05:51:19 +0900

    Feat: API 로직 구현

--- src/app/apis/services/pb.ts
+++ src/app/apis/services/pb.ts
@@ -99,6 +99,33 @@ export const getScheduleInfo = async ({ year, month }: GetScheduleInfoProps) => 
   } catch (error) {}
 };
 
+export const getConsultationStatus = async () => {
+  try {
+    const res = await instance.get("/pb/management/recent");
+    return res.data.data;
+  } catch (error: any) {
+    throw new AxiosError(error.response.data);
+  }
+};
+interface ConsultationListProps {
+  type: string;
+  page: number;
+}
+
+export const getConsultationList = async ({ type, page }: ConsultationListProps) => {
+  try {
+    const res = await instance.get("/pb/management/reservations", {
+      params: {
+        page,
+        type,
+      },
+    });
+    return res.data.data;
+  } catch (error: any) {
+    throw new AxiosError(error.response.data);
+  }
+};
+
 export const getConsultTime = async () => {
   try {
     const res = await instance.get("/pb/consultTime");
@@ -113,6 +140,7 @@ export interface ConsultTimeProps {
   consultEnd: string;
   consultNotice: string;
 }
+
 export const updateConsultTime = async ({ consultStart, consultEnd, consultNotice }: ConsultationTimeCardProps) => {
   try {
     const res = await instance.post("/pb/consultTime", {
@@ -121,6 +149,65 @@ export const updateConsultTime = async ({ consultStart, consultEnd, consultNotic
       consultNotice,
     });
     return res;
+  } catch (error: any) {
+    throw new AxiosError(error.response.data);
+  }
+};
+
+export const getReservationInfo = async ({ id }: { id: number }) => {
+  try {
+    const res = await instance.get(`/pb/reservation/${id}`);
+    return res.data.data;
+  } catch (error: any) {
+    throw new AxiosError(error.response.data);
+  }
+};
+
+interface changeReservationProps {
+  id: number;
+  category: string;
+  time: string;
+  type: string;
+}
+
+export const changeReservation = async ({ id, category, time, type }: changeReservationProps) => {
+  try {
+    const res = await instance.patch(`/pb/reservation/${id}`, {
+      category,
+      time,
+      type,
+    });
+    return res.data.data;
+  } catch (error: any) {
+    throw new AxiosError(error.response.data);
+  }
+};
+
+export const confirmedReservation = async ({ id, time }: { id: number; time: string }) => {
+  try {
+    const res = await instance.patch(`/pb/reservation/${id}/confirmed`, {
+      time: String(time),
+    });
+
+    return res.data.data;
+  } catch (error: any) {
+    throw new AxiosError(error.response.data);
+  }
+};
+
+export const completedReservation = async (id: number) => {
+  try {
+    const res = await instance.patch(`/auth/reservation/${id}/completed`);
+    return res.data.data;
+  } catch (error: any) {
+    throw new AxiosError(error.response.data);
+  }
+};
+
+export const deleteReservation = async (id: number) => {
+  try {
+    const res = await instance.delete(`/auth/reservation/${id}`);
+    return res.data.data;
   } catch (error: any) {
     throw new AxiosError(error.response.data);
   }

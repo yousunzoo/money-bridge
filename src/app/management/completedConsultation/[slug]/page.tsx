0c57004e24c714683992c4a5bc5ea3bb88cb6536
kjun <kjun_all@naver.com>
2023-06-27 04:23:31 +0900

    Feat: 확정된 상담 페이지 API 연결 및 예외처리,에러핸들링

--- src/app/management/completedConsultation/[slug]/page.tsx
+++ src/app/management/completedConsultation/[slug]/page.tsx
@@ -2,103 +2,85 @@
 import UserReservationItem from "@/components/common/Card/CardItem/UserReservationItem";
 import TopNav from "@/components/common/TopNav";
 import React from "react";
-import res from "../../../../mocks/kjun/managementReservations.json";
 import ConsultationLocationSection from "@/components/common/ConsultationLocationSection";
 import ConsultationScheduleSection from "@/components/common/ConsultationScheduleSection";
 import ConsultationNoteSection from "@/components/common/ConsultationNoteSection";
 import DoubleButton from "@/components/common/DoubleButton";
+import { useGetUserInfo } from "@/hooks/useGetUserInfo";
+import { useGetReservationInfo } from "@/hooks/useGetReservationInfo";
+import { redirect, useRouter } from "next/navigation";
+import ErrorModal from "@/components/common/ErrorModal";
+import SingleButton from "@/components/common/SingleButton";
 
-interface Props {
-  params: {
-    slug: string;
-  };
-}
+function CompletedConsultationPage({ params: { slug } }: { params: { slug: number } }) {
+  const router = useRouter();
+  const { userInfo, userLoading, isLogined } = useGetUserInfo();
 
-interface ReservationData {
-  pbId: number;
-  profileImage: string;
-  name: string;
-  phoneNumber: string;
-  reservationId: number;
-  candidateTime1: string;
-  candidateTime2: string;
-  time: string;
-  location: string;
-  locationAddress: string;
-  goal: string;
-  question: string;
-  type: string;
-}
+  if (!isLogined && !userLoading) {
+    redirect("/");
+  }
 
-function CompletedConsultationPage({ params }: Props) {
-  const {
-    pbId,
-    profileImage,
-    name,
-    phoneNumber,
-    reservationId,
-    candidateTime1,
-    candidateTime2,
-    time,
-    location,
-    locationAddress,
-    goal,
-    question,
-    type,
-    reviewCheck,
-  } = res.data.reservationList[0];
-  // profileImage데이터는 api등록 후 UserReservationItem props 내려주고 코드 변경하기
-  const role = "PB";
+  const { reservationInfo, reservationLoading, reservationError } = useGetReservationInfo(slug);
+
+  if (reservationInfo === undefined) return null;
+  const { time, name, phoneNumber, type, location, locationAddress, goal, question, profileImage, reviewCheck } =
+    reservationInfo;
+  if (!userInfo) return;
+  const role = userInfo?.role;
 
   const myReviewClickHandler = () => {
-    console.log("나의 후기");
+    router.push("/management?process=COMPLETE");
   };
   const checkClickHandler = () => {
-    console.log("확인");
+    router.push("/management?process=COMPLETE");
   };
 
-  // 데이터 time 유무에 따라 상담일정, 희망일정 다르게
-  const historyCard = {
-    candidateTime1,
-    candidateTime2,
-    type,
-    time,
-    location,
-    locationAddress,
-    goal,
-    question,
-    role,
-    reviewCheck,
-  };
-  const scheduleSectionProps = { candidateTime1, candidateTime2, role, time };
+  const scheduleSectionProps = { role, time };
   const locationSectionProps = { type, role, location, locationAddress };
   const noteSectionProps = { role, goal, question };
 
+  if (userInfo?.role !== "PB")
+    return (
+      <ErrorModal isError={true} path={"/management?process=APPLY"} content={"권한이 없습니다. 다시 시도해주세요."} />
+    );
+  if (reservationError)
+    return (
+      <ErrorModal
+        isError={true}
+        path={"/management?process=APPLY"}
+        content={"일시적인 문제가 발생했습니다. 다시 시도해주세요."}
+      />
+    );
   return (
     <div>
       <TopNav title="완료된 상담" hasBack={true} />
-      <div className="pb_top_Phrase">
+      <div className="pb_top_Phrase mx-[-16px] mt-4 box-content w-full ">
         <span className="text-white ">상담이 완료되었습니다.</span>
       </div>
-      <UserReservationItem buttonName="고객 정보" href={"/"} isRole={"USER"}>
+      <UserReservationItem buttonName="고객 정보" href={"/"} isRole={"USER"} profileImage={profileImage}>
         <p className="font-bold">{name}</p>
         <p className="text-xs ">{phoneNumber}</p>
         <p className="text-xs ">{type === "VISIT" ? "방문상담" : "유선상담"} </p>
       </UserReservationItem>
-      <section className="mt-6 w-full rounded-md bg-white p-4 pb-6 text-xs">
+      <section className="w-full p-4 pb-6 mt-6 text-xs bg-white rounded-md">
         <ConsultationScheduleSection {...scheduleSectionProps} />
         <ConsultationLocationSection {...locationSectionProps} />
         <ConsultationNoteSection {...noteSectionProps} />
         <div className="flex flex-col items-center pt-6 text-xs">
           <p className="font-bold text-primary-normal">상담이 완료되었습니다.</p>
         </div>
-        <DoubleButton
-          firstTitle={"나의 후기"}
-          secondTitle={"확인"}
-          firstClickFunc={myReviewClickHandler}
-          secondClickFunc={checkClickHandler}
-          role={"PB"}
-        />
+        {reviewCheck ? (
+          <DoubleButton
+            firstTitle={"후기 보러 가기"}
+            secondTitle={"확인"}
+            firstClickFunc={myReviewClickHandler}
+            secondClickFunc={checkClickHandler}
+            role={"PB"}
+            reviewCheck={reviewCheck}
+          />
+        ) : (
+          <SingleButton title={"확인"} role={role} ClickFunc={checkClickHandler} />
+        )}
       </section>
     </div>
   );

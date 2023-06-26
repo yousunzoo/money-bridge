48b4a429949e05f34bd8bc742dc5e39afed76da9
yousunzoo <dpfvmffjqm@gmail.com>
2023-06-26 19:18:48 +0900

    Style: query 관련 함수 타입 지정

--- src/hooks/useEditProfile.ts
+++ src/hooks/useEditProfile.ts
@@ -1,7 +1,16 @@
 import { editPBMyProfile } from "@/app/apis/services/pb";
-import { useMutation } from "@tanstack/react-query";
+import { ILoginedUserInfo } from "@/types/common";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { AxiosError } from "axios";
+import { useRouter } from "next/navigation";
 
 export const useEditProfile = () => {
-  const { mutate } = useMutation(["editProfile"], editPBMyProfile);
+  const router = useRouter();
+  const { id } = useQueryClient().getQueryData(["loginedUserInfo"]) as ILoginedUserInfo;
+  const { mutate } = useMutation<null, AxiosError, FormData>(["editProfile"], editPBMyProfile, {
+    onSuccess: () => {
+      router.push(`/detail/info/${id}`);
+    },
+  });
   return mutate;
 };

9a8c049d02693f66c4eadc03fd311121e20764f3
kjun <kjun_all@naver.com>
2023-06-27 04:24:49 +0900

    Feat: 신규 상담 페이지 API 연결 및 예외처리

--- src/app/management/newReservation/[slug]/page.tsx
+++ src/app/management/newReservation/[slug]/page.tsx
@@ -5,68 +5,34 @@ import React, { useEffect, useState } from "react";
 import ConsultationLocationSection from "@/components/common/ConsultationLocationSection";
 import ConsultationNoteSection from "@/components/common/ConsultationNoteSection";
 import DoubleButton from "@/components/common/DoubleButton";
-import { useMutation, useQuery } from "@tanstack/react-query";
-import { AxiosError } from "axios";
-import { getLoginedUserInfo } from "@/app/apis/services/auth";
+import { useMutation } from "@tanstack/react-query";
 import { redirect, useRouter } from "next/navigation";
-import { changeReservation, confirmedReservation, getReservationInfo } from "@/app/apis/services/pb";
-import { ILoginedUserInfo } from "@/types/common";
+import { confirmedReservation } from "@/app/apis/services/pb";
 import ErrorModal from "@/components/common/ErrorModal";
 import ScheduleSection from "@/components/managementPage/changeReservationPage/ScheduleSection";
-
-interface ReservationData {
-  pbId: number;
-  profileImage: string;
-  name: string;
-  phoneNumber: string;
-  reservationId: number;
-  candidateTime1: string;
-  candidateTime2: string;
-  time: string;
-  location: string;
-  locationAddress: string;
-  goal: string;
-  question: string;
-  type: string;
-  consultEnd: string;
-  consultStart: string;
-  email: string;
-  notice: string;
-  reviewCheck: boolean;
-  userId: number;
-}
+import ButtonModal from "@/components/common/ButtonModal";
+import { useGetUserInfo } from "@/hooks/useGetUserInfo";
+import { useGetReservationInfo } from "@/hooks/useGetReservationInfo";
 
 function NewReservationPage({ params: { slug } }: { params: { slug: number } }) {
   const router = useRouter();
+  const [isButtonOpen, setIsButtonOpen] = useState(false);
   const [isSelectSchedule, setIsSelectSchedule] = useState(true);
   const [timeState, setTimeState] = useState("");
-  const {
-    data: userInfo,
-    isLoading: userLoading,
-    isSuccess: isLogined,
-  } = useQuery<ILoginedUserInfo, AxiosError>({
-    queryKey: ["loginedUserInfo"],
-    queryFn: getLoginedUserInfo,
-    refetchOnWindowFocus: false,
-  });
+
+  const { userInfo, userLoading, isLogined } = useGetUserInfo();
 
   if (!isLogined && !userLoading) {
     redirect("/");
   }
-  const {
-    data: reservationInfo,
-    isLoading: reservationLoading,
-    isError: reservationError,
-  } = useQuery<ReservationData, AxiosError>({
-    queryKey: ["getReservationInfo", slug],
-    queryFn: () =>
-      getReservationInfo({
-        id: slug,
-      }),
-  });
+
+  const { reservationInfo, reservationLoading, reservationError } = useGetReservationInfo(slug);
 
   const { mutate } = useMutation(confirmedReservation, {
-    onSuccess: () => {},
+    onSuccess: data => {
+      console.log(data);
+      router.push(`/management/confirmedReservation/${slug}`);
+    },
   });
 
   // 상담 희망 일정(1순위, 2순위) 선택 버튼
@@ -80,10 +46,19 @@ function NewReservationPage({ params: { slug } }: { params: { slug: number } }) 
   };
 
   const confirmedClickHandler = () => {
-    mutate({
-      id: slug,
-      time: timeState,
-    });
+    setIsButtonOpen(true);
+  };
+
+  const modalContents = {
+    content: "예약 확정을 하시겠습니까?",
+    confirmText: "확인",
+    cancelText: "취소",
+    cancelFn: () => setIsButtonOpen(false),
+    confirmFn: () =>
+      mutate({
+        id: slug,
+        time: timeState,
+      }),
   };
 
   useEffect(() => {
@@ -106,7 +81,6 @@ function NewReservationPage({ params: { slug } }: { params: { slug: number } }) 
     question,
     profileImage,
   } = reservationInfo;
-
   if (!userInfo) return;
   const role = userInfo?.role;
   const formattedPhoneNumber = phoneNumber.replace(/(\d{3})(\d{4})(\d{4})/, "$1-$2-$3");
@@ -121,14 +95,21 @@ function NewReservationPage({ params: { slug } }: { params: { slug: number } }) 
   const noteSectionProps = { role, goal, question };
 
   if (userInfo?.role !== "PB")
-    return <ErrorModal isError={true} path={"/"} content={"권한이 없습니다. 다시 시도해주세요."} />;
+    return (
+      <ErrorModal isError={true} path={"/management?process=APPLY"} content={"권한이 없습니다. 다시 시도해주세요."} />
+    );
   if (reservationError)
-    return <ErrorModal isError={true} path={"/"} content={"일시적인 문제가 발생했습니다. 다시 시도해주세요."} />;
-
+    return (
+      <ErrorModal
+        isError={true}
+        path={"/management?process=APPLY"}
+        content={"일시적인 문제가 발생했습니다. 다시 시도해주세요."}
+      />
+    );
   return (
     <div>
       <TopNav title="신규예약" hasBack={true} />
-      <div className="mt-4 pb_top_Phrase">
+      <div className="pb_top_Phrase mx-[-16px] mt-4 box-content w-full ">
         <span className="text-white ">투자자와 유선으로 상담 일정을 확정해주세요.</span>
       </div>
       <UserReservationItem buttonName="고객 정보" href={"/"} isRole={"USER"} profileImage={profileImage}>
@@ -140,10 +121,9 @@ function NewReservationPage({ params: { slug } }: { params: { slug: number } }) 
         <section className="w-full pb-4 my-4 border-b-1">
           <ScheduleSection {...scheduleSectionProps} />
         </section>
-
         <ConsultationLocationSection {...locationSectionProps} />
         <ConsultationNoteSection {...noteSectionProps} />
-        <div className="flex flex-col items-center pt-6 text-xs">
+        <div className="flex flex-col items-center mt-6 text-xs">
           <p className="font-bold text-primary-normal">투자자와 유선연락을 통해 일정과 장소를 정하신 후</p>
           <p className="text-primary-normal">예약을 확정하시면 투자자에게 상담 확정 알림이 전송됩니다.</p>
         </div>
@@ -154,6 +134,9 @@ function NewReservationPage({ params: { slug } }: { params: { slug: number } }) 
           secondClickFunc={confirmedClickHandler}
           role={"PB"}
         />
+        {isButtonOpen && (
+          <ButtonModal modalContents={modalContents} isOpen={isButtonOpen} setIsOpen={setIsButtonOpen} />
+        )}
       </section>
     </div>
   );

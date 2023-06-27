a84583fc7c4c5b1d75851783d2b950e13edbf95a
kjun <kjun_all@naver.com>
2023-06-28 01:16:23 +0900

    Feat: 관리자 컴포넌트 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import TopNav from "@/components/common/TopNav";
+import AdminButtonSection from "@/components/mainPage/AdminButtonSection";
 import CarouselBanner from "@/components/mainPage/CarouselBanner";
 import CustomListSection from "@/components/mainPage/CustomListSection";
 import PbListSection from "@/components/mainPage/PbListSection";
@@ -9,6 +10,7 @@ import StockFirmSection from "@/components/mainPage/StockFirmSection";
 export default async function Home() {
   return (
     <div>
+      <AdminButtonSection />
       <TopNav title={"메인페이지"} />
       <CarouselBanner />
       <PbListSection />

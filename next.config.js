5661739b0e2ef54eb255ef05f59555022ff014f1
kjun <kjun_all@naver.com>
2023-06-28 01:09:34 +0900

    Feat: 리다이렉트 코드 제거

--- next.config.js
+++ next.config.js
@@ -1,24 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/myCounseling/newReservation",
-        source: "/myCounseling/canceledConsultation",
-        destination: "/myCounseling",
-        permanent: true,
-      },
-      {
-        source: "/management/newReservation",
-        source: "/management/canceledConsultation",
-        source: "/management/changeReservation",
-        source: "/management/completedConsultation",
-        source: "/management/confirmedReservation",
-        destination: "/management",
-        permanent: true,
-      },
-    ];
-  },
   images: {
     domains: ["d2ky5wm6akosox.cloudfront.net"],
   },

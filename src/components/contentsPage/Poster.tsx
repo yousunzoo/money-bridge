bd9ea1b36af980170767f1b8eb30b87549b12784
hyeon17 <janghyeon8725@naver.com>
2023-06-28 02:51:01 +0900

    Feat: 타입 추가

--- src/components/contentsPage/Poster.tsx
+++ src/components/contentsPage/Poster.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import React from "react";
 
-function Poster({ img }: { img: any } ) {
+function Poster({ img }: { img: string } ) {
   return <Image src={img} alt="poster" width={0} height={390} className="mx-auto h-[390px] w-auto" priority={true} />;
 }
 

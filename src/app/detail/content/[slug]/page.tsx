36f81db2916fca9cdae6fbd9e93e278fce3b1ef0
hyeon17 <janghyeon8725@naver.com>
2023-06-29 02:04:36 +0900

    Fix: 버튼 링크 변경, 북마크 여부 수정

--- src/app/detail/content/[slug]/page.tsx
+++ src/app/detail/content/[slug]/page.tsx
@@ -1,21 +1,21 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Intro from "@/components/pbdetailPage/Intro";
 import TopNav from "@/components/common/TopNav";
 import { getLoginedUserInfo } from "@/app/apis/services/auth";
 import { useQuery } from "@tanstack/react-query";
 import { getPbContent, getPbProfile } from "@/app/apis/services/pb";
-import { useRouter, usePathname } from "next/navigation";
+import { usePathname } from "next/navigation";
 import ContentCardList from "@/components/common/Card/CardList/ContentCardList";
 import { getMyId } from "@/utils/pbMyId";
 import { ILoginedUserInfo } from "@/types/common";
 import { AxiosError } from "axios";
 import { IloginProfile } from "@/types/pb";
 import { IDataResponse } from "@/types/common";
+import Link from 'next/link';
 
 function PbDetailContent() {
   const pathname: string = usePathname();
-  const router = useRouter();
   const id: number = Number(pathname.split("/").pop());
   const { data: userData, isLoading } = useQuery<ILoginedUserInfo, AxiosError>({
     queryKey: ["getLoginedUserInfo"],
@@ -32,11 +32,11 @@ function PbDetailContent() {
     <div className="mb-32">
       <TopNav title="PB 상세프로필" hasBack={true} />
       {authProfile?.data && <Intro introData={authProfile?.data} />}
-      <ContentCardList queryKey={`/auth/boards/${id}`} api={getPbContent} etc={id} />
+      <ContentCardList queryKey={`/auth/boards/${id}`} api={getPbContent} etc={id} bookmarks={true} />
       {myId && (
-        <button className="button_fixed" onClick={() => router.push("/contents/write")}>
+        <Link className="button_fixed" href="/contents/write">
           콘텐츠 작성하기
-        </button>
+        </Link>
       )}
     </div>
   );

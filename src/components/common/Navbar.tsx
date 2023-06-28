1661c194e8cec7e22323b19a9868b0e42896632b
kjun <kjun_all@naver.com>
2023-06-28 23:25:59 +0900

    Fix: 로그인 여부에 따른 navbar 수정

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -44,11 +44,10 @@ const navItems = [
 
 function Navbar() {
   const { userInfo, userLoading, isLogined } = useGetUserInfo();
-  if (!userInfo) return;
   return (
     <nav className="fixed bottom-0 left-1/2 flex h-[70px] w-full max-w-[768px] -translate-x-1/2 justify-around bg-white p-2">
       {navItems.map(item => {
-        if (item.role === "ALL" || item.role === userInfo.role) {
+        if (item.role === "ALL" || item.role === userInfo?.role) {
           return (
             <Link key={item.text} href={item.href} className="group flex w-[74px] flex-col items-center">
               <Image src={item.image} alt={item.text} width={26} height={26} />

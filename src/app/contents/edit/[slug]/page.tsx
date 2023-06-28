fb253633d1c648a40f8a3a6fd3557a3b30d4f1d0
hyeon17 <janghyeon8725@naver.com>
2023-06-28 19:17:37 +0900

    Feat: 콘텐츠 수정, 임시저장 컨텐츠 불러오기 추가

--- src/app/contents/edit/[slug]/page.tsx
+++ src/app/contents/edit/[slug]/page.tsx
@@ -1,12 +1,20 @@
+"use client";
 import TopNav from "@/components/common/TopNav";
 import Write from "@/components/contentsPage/Write";
 import React from "react";
+import { getTemp } from "@/app/apis/services/pb";
+import { useQuery } from "@tanstack/react-query";
+import { usePathname } from "next/navigation";
 
 function ContentsEdit() {
+  const pathname: string = usePathname();
+  const id: number = Number(pathname.split("/").pop());
+  const { data: tempData } = useQuery(["getTemp"], () => getTemp(id));
+
   return (
     <>
       <TopNav title="콘텐츠 수정하기" hasBack={true} />
-      {/* <Write /> */}
+      {tempData && <Write data={tempData} id={id} />}
     </>
   );
 }

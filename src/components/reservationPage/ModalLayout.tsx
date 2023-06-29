ab6385ce1cd916d6dd0bf41cf556695aae90e9e1
LeeByoungWook <lbw3973@naver.com>
2023-06-29 17:59:26 +0900

    Design: z-index Up

--- src/components/reservationPage/ModalLayout.tsx
+++ src/components/reservationPage/ModalLayout.tsx
@@ -11,7 +11,7 @@ function ModalLayout({ children, handleCloseModal }: IModalLayoutProps) {
   }, []);
 
   return (
-    <div className="fixed left-0 top-0 z-10 h-full w-full">
+    <div className="fixed left-0 top-0 z-20 h-full w-full">
       <div className="modal_background" />
       <section className="modal flex flex-col">
         <button className="absolute right-6" onClick={handleCloseModal}>

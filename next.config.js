6e6dc5c4a5a57657e5edc2925c711751d3d00a02
LeeByoungWook <lbw3973@naver.com>
2023-07-03 14:57:59 +0900

    Feat: nextConfig에 env 설정 추가

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   images: {
     domains: ["d2ky5wm6akosox.cloudfront.net"],
   },
+  env: {
+    NEXT_PUBLIC_API_KEY: "https://money-bridge.shop:8080/",
+    NEXT_PUBLIC_KAKAO_API_KEY: "a00d33a77c654313f467c84771f981c2",
+  },
 };
 
 module.exports = nextConfig;

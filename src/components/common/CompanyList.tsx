b4043738aab12bebe5676bb2ba34cf144aef739c
yousunzoo <dpfvmffjqm@gmail.com>
2023-07-06 00:54:03 +0900

    Fix: Ant Design 커스터마이징 및 증권사 캐러셀 이전 다음 버튼 추가

--- src/components/common/CompanyList.tsx
+++ src/components/common/CompanyList.tsx
@@ -1,7 +1,8 @@
 import Image from "next/image";
 import { ICompanyListProps } from "@/types/pblist";
-import { Carousel } from "antd";
+import { Carousel, ConfigProvider } from "antd";
 import { chunkArray } from "@/utils/chunkArray";
+import { MdOutlineArrowForwardIos, MdOutlineArrowBackIos } from "react-icons/md";
 import "@/styles/companyCarousel.css";
 
 const LI_STYLE =
@@ -12,35 +13,53 @@ function CompanyList({ companyList, nowCompany, handleIDClick }: ICompanyListPro
   const nowCard = nowCompany === "ALL" ? 0 : Math.ceil((Number(nowCompany) + 1) / 8) - 1;
 
   return (
-    <Carousel dots={true} dotPosition="bottom" draggable={true} initialSlide={nowCard}>
-      {chunkedCompanyList.map((companyList, index) => (
-        <div key={index}>
-          <ul className="grid grid-cols-4 gap-4 px-4">
-            {companyList.map(company => (
-              <li
-                data-id={company.id}
-                onClick={handleIDClick}
-                className={`${LI_STYLE} ${company.id == nowCompany && "bg-primary-normal font-bold text-white"} ${
-                  company.name === "전체보기" && "!justify-center"
-                }`}
-                key={company.id}
-              >
-                {company.logo && <Image src={company.logo} alt={company.name} width={30} height={30} />}
-                {company.name === "전체보기" ? (
-                  <p>
-                    전체
-                    <br />
-                    보기
-                  </p>
-                ) : (
-                  <p className="text-xs font-bold leading-3">{company.name}</p>
-                )}
-              </li>
-            ))}
-          </ul>
-        </div>
-      ))}
-    </Carousel>
+    <ConfigProvider
+      theme={{
+        components: {
+          Carousel: {
+            colorBgContainer: "#242424",
+          },
+        },
+      }}
+    >
+      <Carousel
+        arrows={true}
+        nextArrow={<MdOutlineArrowForwardIos />}
+        prevArrow={<MdOutlineArrowBackIos />}
+        dots={true}
+        dotPosition="bottom"
+        draggable={true}
+        initialSlide={nowCard}
+      >
+        {chunkedCompanyList.map((companyList, index) => (
+          <div key={index}>
+            <ul className="mx-auto grid max-w-[680px] grid-cols-4 gap-4 px-4 pb-6">
+              {companyList.map(company => (
+                <li
+                  data-id={company.id}
+                  onClick={handleIDClick}
+                  className={`${LI_STYLE} ${company.id == nowCompany && "bg-primary-normal font-bold text-white"} ${
+                    company.name === "전체보기" && "!justify-center"
+                  }`}
+                  key={company.id}
+                >
+                  {company.logo && <Image src={company.logo} alt={company.name} width={30} height={30} />}
+                  {company.name === "전체보기" ? (
+                    <p>
+                      전체
+                      <br />
+                      보기
+                    </p>
+                  ) : (
+                    <p className="text-xs font-bold leading-3">{company.name}</p>
+                  )}
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </Carousel>
+    </ConfigProvider>
   );
 }
 

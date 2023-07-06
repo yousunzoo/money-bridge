e5110bf99139f297c800a4b33e83d16db81c10c0
yousunzoo <dpfvmffjqm@gmail.com>
2023-07-06 15:00:05 +0900

    Refactor: TopNav는 layout에서 처리

--- src/app/pblist/page.tsx
+++ src/app/pblist/page.tsx
@@ -13,19 +13,6 @@ function PBListPage() {
 
   return (
     <>
-      <TopNav title="PB 찾기" />
-      <h2 className="mb-7 text-xl font-bold">
-        관심 있는 증권사의
-        <br />
-        전문 PB를 찾아보세요.
-      </h2>
-      <Link
-        href="/pblist/recommend"
-        className="mb-6 flex items-center justify-between rounded-md bg-primary-normal p-4 text-white"
-      >
-        <span>당신을 위한 맞춤 추천</span>
-        <Image src="/assets/images/arrayNext.svg" alt="맞춤 추천으로 이동" width={14} height={14} />
-      </Link>
       <PBMenu />
       <SortTab />
       <FilteredPbCardList />

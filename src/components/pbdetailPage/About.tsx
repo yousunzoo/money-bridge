355a85f43b8b865a52689729a91cdfd104315210
hyeon17 <janghyeon8725@naver.com>
2023-07-08 06:52:57 +0900

    Fix: 필요없는 코드 삭제

--- src/components/pbdetailPage/About.tsx
+++ src/components/pbdetailPage/About.tsx
@@ -53,11 +53,7 @@ function About({ aboutData, role, Id }: { aboutData: IAboutData; role: string; I
     if (role === CommonROLE.USER) {
       router.push(`/reservation?pbId=${id}`);
     } else if (role === CommonROLE.PB) {
-      if (pathname === `/detail/info/${myId}`) {
-        router.push("/my/editProfile");
-      } else {
-        router.push(`/reservation?pbId=${id}`);
-      }
+      if (pathname === `/detail/info/${myId}`) router.push("/my/editProfile");
     }
   };
 
@@ -65,11 +61,7 @@ function About({ aboutData, role, Id }: { aboutData: IAboutData; role: string; I
   if (role === CommonROLE.USER) {
     text = "상담 신청하기";
   } else if (role === CommonROLE.PB) {
-    if (pathname === `/detail/info/${myId}`) {
-      text = "프로필 수정하기";
-    } else {
-      text = "상담 신청하기";
-    }
+    if (pathname === `/detail/info/${myId}`) text = "프로필 수정하기";
   }
   if (!reviewData || !sameData) return null;
   return (

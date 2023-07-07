1d6037b405d0ce2f4680e1448fe749a29a2bade8
kjun <kjun_all@naver.com>
2023-07-07 20:14:36 +0900

    Fix: 모달 오픈시 뒤 컴포넌트 클릭되지 않도록 백그라운드 설정

--- src/components/managementPage/changeReservationPage/CalendarModal.tsx
+++ src/components/managementPage/changeReservationPage/CalendarModal.tsx
@@ -12,25 +12,28 @@ function CalendarModal({
   dateSelectClick,
 }: CalendarModalProps) {
   return (
-    <section className=" fixed bottom-[70px] left-1/2 z-10 flex min-w-[380px]  max-w-[320px] -translate-x-1/2 flex-col items-center rounded-t-md bg-white px-6 py-6 shadow-lg">
-      <div className="flex items-center justify-between w-full mb-4">
-        <h3 className="text-lg font-bold ">
-          날짜를 선택해주세요. <span className="text-xs">(날짜 선택 후 시간 선택)</span>
-        </h3>
-        <button onClick={calendarOpenHandler}>
-          <Image src={close} alt="close" width={24} height={24} />
+    <section className="fixed left-0 top-0 z-20 h-full w-full">
+      <div className="modal_background" />
+      <div className=" fixed bottom-[70px] left-1/2 z-10 flex min-w-[380px]  max-w-[320px] -translate-x-1/2 flex-col items-center rounded-t-md bg-white px-6 py-6 shadow-lg">
+        <div className="mb-4 flex w-full items-center justify-between">
+          <h3 className="text-lg font-bold ">
+            날짜를 선택해주세요. <span className="text-xs">(날짜 선택 후 시간 선택)</span>
+          </h3>
+          <button onClick={calendarOpenHandler}>
+            <Image src={close} alt="close" width={24} height={24} />
+          </button>
+        </div>
+        <SelectCalendar setIsDisabled={setIsDisabled} handleSelect={handleCalendarSelect} />
+        <button
+          onClick={dateSelectClick}
+          className={`w-full rounded-md bg-primary-normal py-3 text-white  ${
+            isDisabled && "inactive"
+          } mt-3  text-base font-bold`}
+          disabled={isDisabled}
+        >
+          선택하기
         </button>
       </div>
-      <SelectCalendar setIsDisabled={setIsDisabled} handleSelect={handleCalendarSelect} />
-      <button
-        onClick={dateSelectClick}
-        className={`w-full rounded-md bg-primary-normal py-3 text-white  ${
-          isDisabled && "inactive"
-        } mt-3  text-base font-bold`}
-        disabled={isDisabled}
-      >
-        선택하기
-      </button>
     </section>
   );
 }
--- src/components/managementPage/changeReservationPage/TimeModal.tsx
+++ src/components/managementPage/changeReservationPage/TimeModal.tsx
@@ -31,28 +31,31 @@ function TimeModal({
   })();
 
   return (
-    <section className="fixed bottom-[70px] left-1/2 z-10 flex min-w-[380px] max-w-[320px] -translate-x-1/2 flex-col  rounded-t-md bg-white px-6 py-6 shadow-lg">
-      <div className="flex items-center justify-between w-full mb-4">
-        <h3 className="text-lg font-bold ">
-          날짜를 선택해주세요. <span className="text-xs">(날짜 선택 후 시간 선택)</span>
-        </h3>
-        <button onClick={timeOpenHandler}>
-          <Image src={close} alt="close" width={24} height={24} />
+    <section className="fixed left-0 top-0 z-20 h-full w-full">
+      <div className="modal_background" />
+      <div className="fixed bottom-[70px] left-1/2 z-10 flex min-w-[380px] max-w-[320px] -translate-x-1/2 flex-col  rounded-t-md bg-white px-6 py-6 shadow-lg">
+        <div className="mb-4 flex w-full items-center justify-between">
+          <h3 className="text-lg font-bold ">
+            날짜를 선택해주세요. <span className="text-xs">(날짜 선택 후 시간 선택)</span>
+          </h3>
+          <button onClick={timeOpenHandler}>
+            <Image src={close} alt="close" width={24} height={24} />
+          </button>
+        </div>
+        <TimeSelect
+          setIsDisabled={setIsDisabled}
+          selectOptions={selectOptions}
+          selectedDate={selectedDate}
+          handleTimeSelect={handleTimeSelect}
+        />
+        <button
+          onClick={timeSelectClick}
+          className={`mt-3 w-full rounded-md bg-primary-normal py-3 text-base font-bold text-white`}
+          disabled={isDisabled}
+        >
+          선택하기
         </button>
       </div>
-      <TimeSelect
-        setIsDisabled={setIsDisabled}
-        selectOptions={selectOptions}
-        selectedDate={selectedDate}
-        handleTimeSelect={handleTimeSelect}
-      />
-      <button
-        onClick={timeSelectClick}
-        className={`mt-3 w-full rounded-md bg-primary-normal py-3 text-base font-bold text-white`}
-        disabled={isDisabled}
-      >
-        선택하기
-      </button>
     </section>
   );
 }

7f75f5653ca2f8595d80522ab3e5a855bdf44175
kjun <kjun_all@naver.com>
2023-07-08 01:41:52 +0900

    chore: 환경변수 설정

--- next.config.js
+++ next.config.js
@@ -3,6 +3,13 @@ const nextConfig = {
   images: {
     domains: ["moneybridge.s3.ap-northeast-2.amazonaws.com"],
   },
+  env: {
+    NEXT_PUBLIC_API_KEY: process.env.NEXT_PUBLIC_API_KEY,
+    NEXT_PUBLIC_KAKAO_API_KEY: process.env.NEXT_PUBLIC_KAKAO_API_KEY,
+    NEXT_PUBLIC_KAKAO_REST_API_KEY: process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY,
+    NEXT_PUBLIC_KAKAO_API_COORD_URL: process.env.NEXT_PUBLIC_KAKAO_API_COORD_URL,
+    NEXT_PUBLIC_KAKAO_API_SEARCH_URL: process.env.NEXT_PUBLIC_KAKAO_API_SEARCH_URL,
+  },
 };
 
 module.exports = nextConfig;

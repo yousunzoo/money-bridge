35192c0b02cee0fd065f19bd94d61118ddd8eda9
kjun <kjun_all@naver.com>
2023-07-09 19:43:09 +0900

    Chore: next.config proxy url 수정

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const nextConfig = {
     domains: ["moneybridge.s3.ap-northeast-2.amazonaws.com"],
   },
   env: {
+    NEXT_PUBLIC_PROXY_API_KEY: process.env.NEXT_PUBLIC_PROXY_API_KEY,
     NEXT_PUBLIC_API_KEY: process.env.NEXT_PUBLIC_API_KEY,
     NEXT_PUBLIC_KAKAO_API_KEY: process.env.NEXT_PUBLIC_KAKAO_API_KEY,
     NEXT_PUBLIC_KAKAO_REST_API_KEY: process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY,
@@ -14,7 +15,7 @@ const nextConfig = {
     return [
       {
         source: "/:path*",
-        destination: `${process.env.NEXT_PUBLIC_PROXY_API_KEY}/:path*`,
+        destination: "https://www.moneybridge.co.kr/:path*",
       },
     ];
   },

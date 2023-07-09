488f71922fab2496eefd801b89b9816691025e78
yousunzoo <dpfvmffjqm@gmail.com>
2023-07-09 20:24:08 +0900

    Chore: 미사용 환경변수 삭제

--- next.config.js
+++ next.config.js
@@ -4,8 +4,6 @@ const nextConfig = {
     domains: ["moneybridge.s3.ap-northeast-2.amazonaws.com"],
   },
   env: {
-    NEXT_PUBLIC_PROXY_API_KEY: process.env.NEXT_PUBLIC_PROXY_API_KEY,
-    NEXT_PUBLIC_API_KEY: process.env.NEXT_PUBLIC_API_KEY,
     NEXT_PUBLIC_KAKAO_API_KEY: process.env.NEXT_PUBLIC_KAKAO_API_KEY,
     NEXT_PUBLIC_KAKAO_REST_API_KEY: process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY,
     NEXT_PUBLIC_KAKAO_API_COORD_URL: process.env.NEXT_PUBLIC_KAKAO_API_COORD_URL,

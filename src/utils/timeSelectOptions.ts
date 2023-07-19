aca4be88c2a68380ec131f139d92dee550c941c9
yousunzoo <dpfvmffjqm@gmail.com>
2023-07-19 15:56:15 +0900

    Fix: 상담 종료 시간이 새벽을 넘어가는 경우의 경우 처리 및 레이아웃 간격 수정

--- src/utils/timeSelectOptions.ts
+++ src/utils/timeSelectOptions.ts
@@ -3,9 +3,21 @@ import dayjs from "dayjs";
 export const timeSelectOptions = ({ consultStart, consultEnd }: { consultStart: string; consultEnd: string }) => {
   const startHour = dayjs(consultStart, "hh").get("hour") || 9;
   const endHour = (dayjs(consultEnd, "hh").get("hour") === 0 ? 24 : dayjs(consultEnd, "hh").get("hour")) || 18;
+  const isLastUntilDawn = endHour < startHour;
+  const endTime = endHour > startHour ? endHour : 24;
+  const options = makeOptions(startHour, endHour);
+  if (isLastUntilDawn) {
+    const dawnOptions = makeOptions(0, endTime);
+    options.am = [...options.am, ...dawnOptions.am];
+    options.pm = [...options.pm, ...dawnOptions.pm];
+  }
+  return options;
+};
+
+function makeOptions(start: number, end: number) {
   const am = [];
   const pm = [];
-  for (let i = startHour; i <= endHour; i++) {
+  for (let i = start; i <= end; i++) {
     if (i < 12) {
       am.push(i < 10 ? `0${i}:00` : `${i}:00`);
     } else {
@@ -13,4 +25,4 @@ export const timeSelectOptions = ({ consultStart, consultEnd }: { consultStart: 
     }
   }
   return { am, pm };
-};
+}

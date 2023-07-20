2e6a6f2262a85964319a5211c53d0a5bce8fd7e4
kjun <kjun_all@naver.com>
2023-07-20 15:29:06 +0900

    Feat: 네이버 애널리틱스 테스트

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -91,13 +91,27 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <Script
           src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_API_KEY}&autoload=false`}
         />
+        <Script src="//wcs.naver.net/wcslog.js" strategy="afterInteractive" />
+        <Script
+          id="naver-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            if (!wcs_add) var wcs_add = {};
+            wcs_add["wa"] = "1c72383b4c03d20";
+            if (window.wcs) {
+              wcs_do();
+            }
+          `,
+          }}
+        />
       </head>
       <body className={noto.className}>
         <ReactQueryProvider>
           <Suspense>
             <Main>
               <TopNav />
-              <div className="mb-40 flex-1">{children}</div>
+              <div className="flex-1 mb-40">{children}</div>
               <Footer />
               <Navbar />
             </Main>

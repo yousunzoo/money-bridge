3c84f27a76de7ecc680ffcad5b5baa20d8f32046
kjun <kjun_all@naver.com>
2023-08-09 21:46:38 +0900

    Feat: kakao script 동적 추가 구현

---
+++ src/components/joinPage/pb/kakaoMap/KakaoMapScriptLoader.tsx
@@ -0,0 +1,39 @@
+import React, { ReactNode, useEffect, useState } from "react";
+
+const KAKAO_MAP_ID = "kakao-map-script";
+
+function KakaoMapScriptLoader({ children }: { children: ReactNode }) {
+  const [mapScriptLoaded, setMapScriptLoaded] = useState(false);
+  useEffect(() => {
+    const mapScript = document.getElementById(KAKAO_MAP_ID);
+    if (mapScript && !window.kakao) return;
+
+    const script = document.createElement("script");
+    script.id = KAKAO_MAP_ID;
+    script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_API_KEY}&libraries=services&autoload=false`;
+    script.onload = () => {
+      window.kakao.maps.load(() => {
+        setMapScriptLoaded(true);
+      });
+    };
+    script.onerror = () => {
+      setMapScriptLoaded(false);
+    };
+
+    document.getElementById("root")?.appendChild(script);
+  }, []);
+
+  return (
+    <>
+      {mapScriptLoaded ? (
+        children
+      ) : (
+        <div>
+          <h1>지도를 불러오는 중입니다.</h1>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default KakaoMapScriptLoader;

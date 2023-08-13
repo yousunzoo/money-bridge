647870395717b6ff05b25f2125e81f66575b0c13
kjun <kjun_all@naver.com>
2023-08-14 02:32:16 +0900

    feat: MapMarkerController 컴포넌트 추가

---
+++ src/components/joinPage/pb/kakaoMap/MapMarkerController.tsx
@@ -0,0 +1,35 @@
+"use client";
+import { PlaceType } from "@/types/mapTypes";
+import React, { useEffect } from "react";
+import MapMarker from "./MapMarker";
+import useMapStore from "@/store/kakaoMapStore";
+
+interface MapMarkerControllerProps {
+  places: PlaceType[];
+}
+
+function MapMarkerController(props: MapMarkerControllerProps) {
+  const { kakaoMap } = useMapStore();
+
+  useEffect(() => {
+    if (props.places.length < 1) {
+      return;
+    }
+
+    const bounds = new window.kakao.maps.LatLngBounds();
+    props.places.forEach(place => {
+      bounds.extend(place.position);
+    });
+
+    kakaoMap && kakaoMap.setBounds(bounds);
+  }, [props.places]);
+  return (
+    <>
+      {props.places.map(place => {
+        return <MapMarker key={place.id} place={place} />;
+      })}
+    </>
+  );
+}
+
+export default MapMarkerController;

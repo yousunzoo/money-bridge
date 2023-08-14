e58dc9200b953b99c358e1d100ea1f5dcb3ca46e
kjun <kjun_all@naver.com>
2023-08-14 19:48:48 +0900

    Feat: SingleInputForm 연결 - 비밀번호 찾기시 이름작성 제외

--- src/components/findPasswordPage/EnterInformation.tsx
+++ src/components/findPasswordPage/EnterInformation.tsx
@@ -1,11 +1,11 @@
-import DoubleInputForm from "@/components/common/DoubleInputForm";
 import { InputFormType } from "@/constants/enum";
+import SingleInputForm from "../common/SingleInputForm";
 
 function EnterInformation() {
   return (
     <>
       <p className="mb-10 mt-14 text-xl font-bold leading-7">가입할 때 등록한 정보를 입력해주세요.</p>
-      <DoubleInputForm type={InputFormType.FIND_PASSWORD} />
+      <SingleInputForm type={InputFormType.FIND_PASSWORD} />
     </>
   );
 }

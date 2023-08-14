54953a55e35d8aa96f8c1e9a0be374f7430b6384
yousunzoo <dpfvmffjqm@gmail.com>
2023-08-14 12:12:56 +0900

    Refactor: 라운지 페이지 동적 라우팅 처리

--- src/components/loungePage/LoungeHot.tsx
+++ src/components/loungePage/LoungeHot.tsx
@@ -1,13 +1,10 @@
 "use client";
+
 import { getLoungeHot as Hot } from "@/app/apis/services/common";
 import ContentCardList from "@/components/common/Card/CardList/ContentCardList";
 
 function LoungeHot() {
-  return (
-    <>
-      <ContentCardList queryKey={"getLoungeHot"} api={Hot} bookmarks={false} />
-    </>
-  );
+  return <ContentCardList queryKey={"getLoungeHot"} api={Hot} bookmarks={false} />;
 }
 
 export default LoungeHot;

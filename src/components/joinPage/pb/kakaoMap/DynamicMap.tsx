5e0efbceb46a7d067fd494051bb08d8e19663d4a
kjun <kjun_all@naver.com>
2023-08-17 02:46:06 +0900

    Feat: 기존 위치 위도, 경도 수정

--- src/components/joinPage/pb/kakaoMap/DynamicMap.tsx
+++ src/components/joinPage/pb/kakaoMap/DynamicMap.tsx
@@ -1,13 +1,13 @@
 import useMapStore from "@/store/kakaoMapStore";
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef } from "react";
 
 function DynamicMap({ children }: { children: React.ReactNode }) {
   const { kakaoMap, setKakaoMap } = useMapStore();
   const kakaoMapRef = useRef<HTMLDivElement>(null);
+
   useEffect(() => {
     if (!kakaoMapRef.current) return;
-
-    const targetPoint = new kakao.maps.LatLng(33.450701, 126.570667);
+    const targetPoint = new kakao.maps.LatLng(37.498095, 127.02761);
     const options = {
       center: targetPoint,
       level: 3,
@@ -17,7 +17,7 @@ function DynamicMap({ children }: { children: React.ReactNode }) {
 
   return (
     <div className="h-[220px]">
-      <div className="w-full h-full " ref={kakaoMapRef}>
+      <div className="h-full w-full " ref={kakaoMapRef}>
         {kakaoMap ? <>{children}</> : <div>지도 정보를 가져오는데 실패하였습니다.</div>}
       </div>
     </div>

464ee532a25cca006a59e1e1cb1156e613bc6cf0
kjun <kjun_all@naver.com>
2023-08-18 18:39:56 +0900

    Feat: 개인정보수정 페이지 레이아웃 생성 - 페이지 revalidate 설정

---
+++ src/app/my/layout.tsx
@@ -0,0 +1,9 @@
+import React, { ReactNode } from "react";
+
+export const revalidate = 0;
+
+function MyLayout({ children }: { children: ReactNode }) {
+  return <>{children}</>;
+}
+
+export default MyLayout;

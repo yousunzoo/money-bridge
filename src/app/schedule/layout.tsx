a004364e1230aff2d4331b596f2763adf2d72371
kjun <kjun_all@naver.com>
2023-08-18 18:43:37 +0900

    Feat: 일정관리 페이지 revalidate 설정

--- src/app/schedule/layout.tsx
+++ src/app/schedule/layout.tsx
@@ -5,6 +5,8 @@ export const metadata: Metadata = {
   title: "일정 관리",
 };
 
+export const revalidate = 0;
+
 function SchduleLayout({ children }: { children: ReactNode }) {
   return <>{children}</>;
 }
